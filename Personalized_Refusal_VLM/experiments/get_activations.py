d264caa7536409ae0ed979165e4ae9d9e54528f4
autosave <local@autosave>
2025-11-16 17:03:12 -0500

    auto-save 17:03:12

--- Personalized_Refusal_VLM/experiments/get_activations.py
+++ Personalized_Refusal_VLM/experiments/get_activations.py
@@ -164,9 +164,7 @@ def eval_model(args):
     # torch.save(with_sys_image_others_activations, f"{save_path}/with_sys_image_others_activations_{cfg.model_name}.pt")
     # torch.save(without_sys_image_biology_activations, f"{save_path}/without_sys_image_biology_activations_{cfg.model_name}.pt")
 
-    pbar = tqdm(range(-5, 0), total=5, desc="Saving activations at different positions")
-    for i in pbar:
-        pbar.set_description(f"Saving activations at position {i}")
+    for i in tqdm(range(-5,0), desc=f"Saving activations", total=5):
         with_sys_out_train_activations = process(get_activations(cfg, model, with_sys_out_train_text, out_train_images, processor, system_prompt=False, pos=i))
         torch.save(with_sys_out_train_activations, f"{save_path}/with_sys_out_train_activations_{cfg.model_name}_pos_{i}.pt")
 
