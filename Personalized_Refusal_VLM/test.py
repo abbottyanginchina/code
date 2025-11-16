9e04ca28af9a97f87fb7fb9dfbdd8ec51f3a2790
autosave <local@autosave>
2025-11-15 19:40:08 -0500

    auto-save 19:40:08

--- Personalized_Refusal_VLM/test.py
+++ Personalized_Refusal_VLM/test.py
@@ -1,3 +1,3 @@
 from datasets import load_dataset
 
-dataset = load_dataset(f"/gpu02home/jmy5701/gpu/data/ScienceQA")["train"]
+dataset = load_dataset(f"/gpu02home/jmy5701/gpu/data/ScienceQA")["train"].filter(lambda e: e["image"] is not None)
