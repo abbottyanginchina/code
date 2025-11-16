eae9d9612c7feaeb47e462ad3c15137cb64af4dd
autosave <local@autosave>
2025-11-15 22:21:22 -0500

    auto-save 22:21:22

--- Personalized_Refusal_VLM/test.py
+++ Personalized_Refusal_VLM/test.py
@@ -28,22 +28,9 @@ messages = [
         "role": "user",
         "content": [
             {"type": "image"},
-            {"type": "text", "text": "What do we see in this image?"},
+            {"type": "text", "text": "Say 'I cannot answer' if the question is NOT related to 'physics'. Question: What do we see in this image?"},
         ]
     },
-    {
-        "role": "assistant",
-        "content": [
-            {"type": "text", "text": "In this image, we can see the city of New York, and more specifically the Statue of Liberty."},
-        ]
-    },
-    {
-        "role": "user",
-        "content": [
-            {"type": "image"},
-            {"type": "text", "text": "And how about this image?"},
-        ]
-    },       
 ]
 prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
 inputs = processor(text=prompt, images=[image1, image2], return_tensors="pt")
