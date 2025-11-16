f77ecb11e902ce293b3dfecb63e634229875032b
autosave <local@autosave>
2025-11-15 21:16:00 -0500

    auto-save 21:16:00

--- Personalized_Refusal_VLM/test.py
+++ Personalized_Refusal_VLM/test.py
@@ -1,21 +1,23 @@
 import requests
 from PIL import Image
+
 import torch
 from transformers import AutoProcessor, LlavaForConditionalGeneration
 
-model_id = "/gpuhome/jmy5701/gpu/models/llava-1.5-7b-hf"
-
+model_id = "llava-hf/llava-1.5-7b-hf"
 model = LlavaForConditionalGeneration.from_pretrained(
-    model_id,
-    torch_dtype=torch.float16,
-    device_map="auto",
-    low_cpu_mem_usage=True,
-)
+    model_id, 
+    torch_dtype=torch.float16, 
+    low_cpu_mem_usage=True, 
+).to(0)
 
 processor = AutoProcessor.from_pretrained(model_id)
 
+# Define a chat history and use `apply_chat_template` to get correctly formatted prompt
+# Each value in "content" has to be a list of dicts with types ("text", "image") 
 conversation = [
     {
+
       "role": "user",
       "content": [
           {"type": "text", "text": "What are these?"},
@@ -23,29 +25,11 @@ conversation = [
         ],
     },
 ]
-
 prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-image_url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-raw_image = Image.open(requests.get(image_url, stream=True).raw).convert("RGB")
+image_file = "http://images.cocodataset.org/val2017/000000039769.jpg"
+raw_image = Image.open(requests.get(image_file, stream=True).raw)
+inputs = processor(images=raw_image, text=prompt, return_tensors='pt').to(0, torch.float16)
 
-processed = processor(
-    images=raw_image,
-    text=prompt,
-    return_tensors='pt',
-    torch_dtype=torch.float16,
-)
-
-print("processed dtypes:")
-for k, v in processed.items():
-    print(k, v.dtype)
-import pdb; pdb.set_trace()
-
-inputs = {}
-for k, v in processed.items():
-    if k == "pixel_values":
-        # 图片才转成 fp16
-        inputs[k] = v.to("cuda", dtype=torch.float16, non_blocking=True)
-    else:
-        # input_ids & attention_mask 保持 long / int 类型，不要转 dtype
-        inputs[k] = v.to("cuda", non_blocking=True)
+output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+print(processor.decode(output[0][2:], skip_special_tokens=True))
