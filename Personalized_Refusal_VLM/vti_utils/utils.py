a481b89c63cf3de37be3254ffd298f8aea775a71
autosave <local@autosave>
2025-11-15 19:26:25 -0500

    auto-save 19:26:25

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -232,7 +232,7 @@ def get_all_datasets_filter(args):
     in_domain = ['physics']
     out_of_domain = ['biology', 'geography', 'writing-strategies', 'figurative-language', 'economics', 'earth-science']
 
-    dataset = load_dataset(f"/gpu02home/jmy5701/gpu/data/ScienceQA_hf")["train"].filter(lambda e: e["image"] is not None)
+    dataset = load_dataset(f"/gpu02home/jmy5701/gpu/data/ScienceQA")["train"].filter(lambda e: e["image"] is not None)
     # dataset = load_dataset(f"/root/autodl-fs/ScienceQA")["train"].filter(lambda e: e["image"] is not None)
     import pdb; pdb.set_trace()
 
