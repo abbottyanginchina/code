d8a819e4801c524683058d9e3abd2f6ca51309a2
autosave <local@autosave>
2025-11-17 14:42:05 -0500

    auto-save 14:42:05

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -36,7 +36,7 @@ class VTILayer(nn.Module):
         K=0
         for i in range(K):
             pos = T - K + i                 # 最后 K 个 token 的位置
-            steer = F.normalize(v[i], dim=-1).view(1, 1, H)
+            steer = F.normalize(self.vti_direction[:, i, :], dim=-1)
             x_new[:, pos, :] = x_float[:, pos, :] + 0.1 * steer
             # ⚠️ 注意：这里不加到 x_new，而是直接覆盖（用 x_float 原来的）
 
