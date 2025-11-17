ab8efe5b0e6030effa64ee19113a255f0802e09c
autosave <local@autosave>
2025-11-17 14:07:23 -0500

    auto-save 14:07:23

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -272,7 +272,6 @@ def add_multiple_layers(model: PreTrainedModel, vti_directions: Tensor, alpha: l
         mlp_keywords = ["mlp", "feedforward", "ffn"]
         # assert len(vti_directions) == len(layers)
         for idx in layer_indices:
-            import pdb; pdb.set_trace()
             layer = layers[idx]
             original_mlp = find_module(layer, mlp_keywords)
             layer.mlp = nn.Sequential(original_mlp, VTILayer(vti_directions[idx], alpha))
