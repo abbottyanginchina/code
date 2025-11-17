b35ea077d219947f0fe115b08c2d81e7bbdfb660
autosave <local@autosave>
2025-11-16 20:44:53 -0500

    auto-save 20:44:53

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -23,8 +23,9 @@ class VTILayer(nn.Module):
                     y += self.lam[i] * lambda_sim * F.normalize(self.vti_direction[i], dim=-1).repeat(1,x.shape[1],1)
                 else:
                     lambda_sim = 1.0
-                    import pdb; pdb.set_trace()
+                    
                     y += self.lam[i] * lambda_sim * F.normalize(self.vti_direction[i], dim=-1)
+            import pdb; pdb.set_trace()
             y = y/len(self.vti_direction)
             x = F.normalize(F.normalize(x.float(),dim=-1) +  0.1 * y, dim=-1) * norm
                 
