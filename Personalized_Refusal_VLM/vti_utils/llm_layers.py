9b756d5deecc46dd59af095ef0d6f6d834ab5d13
autosave <local@autosave>
2025-11-16 23:41:17 -0500

    auto-save 23:41:17

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -31,6 +31,7 @@ class VTILayer(nn.Module):
             # x: torch.Size([1, 595, 4096]) y: torch.Size([4096]) norm: torch.Size([1, 595, 1])
             x = F.normalize(F.normalize(x.float(),dim=-1) +  0.1 * y, dim=-1) * norm 
 
+            # x: torch.Size([1, 595, 4096])
             import pdb; pdb.set_trace()   
                 
             return x.half()
