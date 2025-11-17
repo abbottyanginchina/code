aeb24ebe4e8717f0f67c2d1254e63bbc1b9862a7
autosave <local@autosave>
2025-11-17 14:25:01 -0500

    auto-save 14:25:01

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -90,6 +90,8 @@ class VTILayer(nn.Module):
         if self.vti_direction is not None:
             norm = torch.norm(x.float(),dim=-1).unsqueeze(-1)            
             y = 0
+            
+            y += self.lam[i] * lambda_sim * F.normalize(self.vti_direction[:, -1, :], dim=-1)
             # for i in range(len(self.vti_direction)):
             #     if x.size(1) < 2:
             #         lambda_sim = 1.0 #+ torch.max(torch.tensor([0.]).to(x.device), F.cosine_similarity(x.float(), -self.vti_direction[i][None,None,:], dim=-1)).unsqueeze(-1)
