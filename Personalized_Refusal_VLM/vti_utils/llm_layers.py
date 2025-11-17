d076587e6603c29738f5aff84bdbb2e8eaa27d03
autosave <local@autosave>
2025-11-16 21:25:35 -0500

    auto-save 21:25:35

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -26,10 +26,20 @@ class VTILayer(nn.Module):
                     y += self.lam[i] * lambda_sim * F.normalize(self.vti_direction[i], dim=-1)
             y = y/len(self.vti_direction)
 
-            x = F.normalize(F.normalize(x[:, x.size(1)-10:x.size(1), :].float(),dim=-1) +  0.1 * y, dim=-1) 
-            import pdb; pdb.set_trace()
-            x = x * norm[:, -10:, :]
-                
+            # ---- 只操作最后10个position ----
+            x_last = x[:, x.size(1)-10:x.size(1), :]            # [B,10,H]
+
+            x_last = F.normalize(
+                        F.normalize(x_last.float(), dim=-1) + 0.1 * y,
+                        dim=-1
+                    )
+
+            x_last = x_last * norm[:, -10:, :]                 # [B,10,1]
+
+            # ---- 写回原 x ----
+            x = x.clone()
+            x[:, -10:, :] = x_last.to(x.dtype)
+
             return x.half()
         else:
             return x
