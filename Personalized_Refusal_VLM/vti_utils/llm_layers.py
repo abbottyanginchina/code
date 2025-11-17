459a49c3bce2e50551b7a45e54b9c7cd6f1a1d6c
autosave <local@autosave>
2025-11-17 14:00:40 -0500

    auto-save 14:00:40

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -45,7 +45,7 @@ class VTILayer(nn.Module):
         # K = v.size(0)
         # K = min(K, T)
 
-        K=0
+        K=10
         for i in range(K):
             pos = T - K + i                 # 最后 K 个 token 的位置
             steer = F.normalize(v[i], dim=-1).view(1, 1, H)
