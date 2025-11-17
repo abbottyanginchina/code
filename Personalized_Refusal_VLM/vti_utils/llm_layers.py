3b6f5ce61c3926f536b3b41e9bfb3e654f7c460c
autosave <local@autosave>
2025-11-16 21:18:18 -0500

    auto-save 21:18:18

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -30,7 +30,7 @@ class VTILayer(nn.Module):
 
             x = F.normalize(F.normalize(x[:, x.size(1)-10:x.size(1), :].float(),dim=-1) +  0.1 * y, dim=-1) 
             import pdb; pdb.set_trace()
-            x = x * norm
+            x = x * norm[:, -10:, :]
                 
             return x.half()
         else:
