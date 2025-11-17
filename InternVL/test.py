ff9b864ad12c6cd2a2d77b1c9de67be0cd5039e3
autosave <local@autosave>
2025-11-17 15:58:55 -0500

    auto-save 15:58:55

--- InternVL/test.py
+++ InternVL/test.py
@@ -11,7 +11,7 @@ model = AutoModel.from_pretrained(
     torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
     use_flash_attn=True,
-    trust_remote_code=True
+    # trust_remote_code=True
 ).eval().cuda()
 
 # ====== 2. 加载图像 → pixel_values ======
