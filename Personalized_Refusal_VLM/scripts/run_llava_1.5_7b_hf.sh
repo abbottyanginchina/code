52fc4a224adcdf19772eac7697065d57f66bd3ee
autosave <local@autosave>
2025-11-17 14:07:17 -0500

    auto-save 14:07:17

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -7,7 +7,7 @@ num_train=200
 num_test=100
 model_name="llava-1.5-7b-hf"
 inter_start_layer=12
-inter_end_layer=32
+inter_end_layer=31
 alpha_text=1.8
 
 # echo "🚀 Step 1: Extracting activations..."
