8b0ef2043cb218ae273e7d141cd4183c502d8e72
autosave <local@autosave>
2025-11-18 10:48:01 -0500

    auto-save 10:48:01

--- InternVL/test2.py
+++
@@ -1,8 +0,0 @@
-from llava.conversation import conv_templates
-from llava.mm_utils import (
-    KeywordsStoppingCriteria,
-    expand2square,
-    get_model_name_from_path,
-    load_pretrained_model,
-    tokenizer_image_token,
-)
