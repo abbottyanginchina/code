909ebe999aabf4abc424ca7eb198d8d5a804e61f
autosave <local@autosave>
2025-11-19 15:26:27 -0500

    auto-save 15:26:27

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -13,7 +13,7 @@ alpha_text=1.8
 dataset="ScienceQA"
 
 # echo "🚀 Step 1: Extracting activations..."
-# python -m experiments.get_activations --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset
+python -m experiments.get_activations --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset
 
 # echo "🧠 Step 2: Training steering vector model..."
 # python -m experiments.train_steering_vector --model_name $model_name --start_layer 9 --end_layer 10 &
