d3333bc24e8bff1770d304d2a0d97096aba9f889
autosave <local@autosave>
2025-11-20 15:06:22 -0500

    auto-save 15:06:22

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -411,6 +411,8 @@ def get_demos(args, image_processor, model, tokenizer, patch_size = 14, file_pat
     
     return inputs_images, input_ids
 
+def get_activations_qwen(model, inputs_text, image, tokenizer, system_prompt=False):
+
 def get_activations(model, inputs_text, image, processor, system_prompt=False):
     # import pdb; pdb.set_trace()
     h_all = []
@@ -430,7 +432,8 @@ def get_activations(model, inputs_text, image, processor, system_prompt=False):
                         {
                         "role": "system",
                         "content": [
-                            {"type": "text", "text": "Say 'I cannot answer that question.' directly if you refuse to answer."},
+                            {"type": "text", 
+                             "text": "Say 'I cannot answer that question.' directly if you refuse to answer."},
                             ],
                         },
                         {
