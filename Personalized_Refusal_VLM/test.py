1e03bc5d500491f9fc9525c20dfc3d3473562307
autosave <local@autosave>
2025-11-20 16:20:36 -0500

    auto-save 16:20:36

--- Personalized_Refusal_VLM/test.py
+++ Personalized_Refusal_VLM/test.py
@@ -3,7 +3,7 @@ import torch
 
 # 加载分词器和模型
 model_name = "/gpuhome/jmy5701/gpu/models/Qwen-VL-Chat"  # 或者 "Qwen/Qwen-Chat"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=True)
 
 # 设置模型为评估模式
