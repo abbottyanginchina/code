03f76b30234f7db76b175421bcf304ab2b4c0f2e
autosave <local@autosave>
2025-11-23 16:17:50 -0500

    auto-save 16:17:50

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -230,7 +230,8 @@ def get_all_datasets(args):
 
 def get_all_datasets_filter(args):
     if args.data.dataset_name == "ScienceQA":
-        in_domain = ['physics']
+        in_domain = []
+        in_domain.append(args.subject)
         out_of_domain = ['biology', 'geography', 'writing-strategies', 'figurative-language', 'economics', 'earth-science']
         # dataset = load_dataset(f"{args.model_path}/ScienceQA")["train"].filter(lambda e: e["image"] is not None)
         dataset = load_dataset(f"{args.data.path}/ScienceQA")["train"].filter(lambda e: e["image"] is not None)
