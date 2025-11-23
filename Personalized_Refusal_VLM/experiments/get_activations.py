065cb7bcf6e2ce0051169c21b7acfa1629cc5e3e
autosave <local@autosave>
2025-11-23 17:35:41 -0500

    auto-save 17:35:41

--- Personalized_Refusal_VLM/experiments/get_activations.py
+++ Personalized_Refusal_VLM/experiments/get_activations.py
@@ -199,7 +199,7 @@ def parse_args():
     parser.add_argument(
         "--model_path",
         type=str,
-        default="/gpuhome/jmy5701/gpu/models",
+        default="/gpu02home/jmy5701/gpu/models",
         help="Path to the pretrained models",
     )
     parser.add_argument(
