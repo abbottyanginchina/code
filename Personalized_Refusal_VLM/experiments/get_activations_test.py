c4100cd246ed0f210fbd1f4ace35ce4ef0d0fda2
autosave <local@autosave>
2025-11-24 15:00:45 -0500

    auto-save 15:00:45

--- Personalized_Refusal_VLM/experiments/get_activations_test.py
+++ Personalized_Refusal_VLM/experiments/get_activations_test.py
@@ -111,7 +111,7 @@ def eval_model(cfg):
             output_hidden_states=True,       
             torch_dtype=torch.float16
         ).to(device).eval()
-    elif 'idefics3' in model_path.lower():
+    elif 'idefics3-' in model_path.lower():
         model = AutoModelForVision2Seq.from_pretrained(
             model_path,
             output_hidden_states=True,       
