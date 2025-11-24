b3bf12a7755cef1e4e95f6026cb64559fc86da65
autosave <local@autosave>
2025-11-23 23:21:29 -0500

    auto-save 23:21:29

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -19,7 +19,7 @@ echo "🚀 Step 1: Extracting activations..."
 python -m experiments.train_steering_vector --model_name $model_name --start_layer 9 --end_layer 10 &
 # python -m experiments.train_steering_vector --model_name $model_name --start_layer 18 --end_layer 20 &
 # python -m experiments.train_steering_vector --model_name $model_name --start_layer 28 --end_layer 30 &
-# wait
+wait
 
 
 # for ((layer=0; layer<num_layers; layer+=step)); do
