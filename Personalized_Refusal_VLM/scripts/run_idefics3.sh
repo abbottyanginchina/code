d93265c9489f9d6d746796a723c7cbdd5ad16795
autosave <local@autosave>
2025-11-24 21:06:25 -0500

    auto-save 21:06:25

--- Personalized_Refusal_VLM/scripts/run_idefics3.sh
+++ Personalized_Refusal_VLM/scripts/run_idefics3.sh
@@ -11,6 +11,7 @@ inter_start_layer=18
 inter_end_layer=32
 alpha_text=1.
 dataset="ScienceQA"
+subject="biology"
 
 echo "🚀 Step 1: Extracting activations..."
 python -m experiments.get_activations_inst --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset
