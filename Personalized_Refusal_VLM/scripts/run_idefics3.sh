8cbd4040290aec37ac8cbb1744e4e689acd20def
autosave <local@autosave>
2025-11-24 19:23:42 -0500

    auto-save 19:23:42

--- Personalized_Refusal_VLM/scripts/run_idefics3.sh
+++ Personalized_Refusal_VLM/scripts/run_idefics3.sh
@@ -9,7 +9,7 @@ num_test=100
 model_name="Idefics3-8B-Llama3"
 inter_start_layer=12
 inter_end_layer=32
-alpha_text=2.5
+alpha_text=1.5
 dataset="ScienceQA"
 
 echo "🚀 Step 1: Extracting activations..."
