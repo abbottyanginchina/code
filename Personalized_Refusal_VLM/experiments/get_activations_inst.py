65cbd70eb6c2b07f66c7f150dcaa3071517b171f
autosave <local@autosave>
2025-11-26 17:48:03 -0500

    auto-save 17:48:03

--- Personalized_Refusal_VLM/experiments/get_activations_inst.py
+++ Personalized_Refusal_VLM/experiments/get_activations_inst.py
@@ -158,7 +158,7 @@ def eval_model(cfg):
         with_sys_in_train_activations = process(get_activations_inst(cfg, model, with_sys_in_train_text, in_train_images, processor, system_prompt=False))
         with_sys_out_train_activations = process(get_activations_inst(cfg, model, with_sys_out_train_text, out_train_images, processor, system_prompt=True))
         without_sys_in_train_activations = process(get_activations_inst(cfg, model, without_sys_in_train_text, in_train_images, processor, system_prompt=False))
-        without_sys_out_train_activations = process(get_activations_inst(cfg, model, without_sys_out_train_text, out_train_images, processor, system_prompt=True))
+        without_sys_out_train_activations = process(get_activations_inst(cfg, model, without_sys_out_train_text, out_train_images, processor, system_prompt=False))
 
         # 1. 加 system prompt 的 others（对应 h_c(Image_{others} + system_prompt)）
         with_sys_image_others_activations = process(
