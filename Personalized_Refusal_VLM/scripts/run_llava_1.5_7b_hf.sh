4202804c227ce0a3413fac25cc1640583ec9d61c
autosave <local@autosave>
2025-11-26 15:03:52 -0500

    auto-save 15:03:52

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -78,6 +78,9 @@ wait
 # done
 # wait
 
+python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train --inter_start_layer $inter_start_layer \
+    --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
+
 
 # python -m experiments.get_activations --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject "geography"
 
