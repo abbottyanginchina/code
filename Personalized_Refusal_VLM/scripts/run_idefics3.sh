6a0365eca92a26bdba7ba966ca765265a6d36c29
autosave <local@autosave>
2025-11-26 17:51:29 -0500

    auto-save 17:51:29

--- Personalized_Refusal_VLM/scripts/run_idefics3.sh
+++ Personalized_Refusal_VLM/scripts/run_idefics3.sh
@@ -36,7 +36,7 @@ done
 wait
 
 echo "🎯 Step 3: Inference activations with steering vectors applied..."
-# python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --model_name $model_name
+python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --model_name $model_name
 
 echo "🎉 Step 4: Generating responses with steering vectors applied..."
 python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train --inter_start_layer $inter_start_layer \
