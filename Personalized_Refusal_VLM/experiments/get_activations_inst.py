a8427b707ddbe3a52976f22ebbc7226f7de05e85
autosave <local@autosave>
2025-11-26 17:55:01 -0500

    auto-save 17:55:01

--- Personalized_Refusal_VLM/experiments/get_activations_inst.py
+++ Personalized_Refusal_VLM/experiments/get_activations_inst.py
@@ -155,7 +155,7 @@ def eval_model(cfg):
     print('Obtaining direction\n')
 
     with torch.no_grad():
-        with_sys_in_train_activations = process(get_activations(model, with_sys_in_train_text, in_train_images, processor, system_prompt=False))
+        with_sys_in_train_activations = process(get_activations(model, with_sys_in_train_text, in_train_images, processor, system_prompt=True))
         with_sys_out_train_activations = process(get_activations_inst(cfg, model, with_sys_out_train_text, out_train_images, processor, system_prompt=True))
         without_sys_in_train_activations = process(get_activations(model, without_sys_in_train_text, in_train_images, processor, system_prompt=False))
         without_sys_out_train_activations = process(get_activations(model, without_sys_out_train_text, out_train_images, processor, system_prompt=False))
