804a6df0c016e101e2bd51ec001f54c2bc3d7f10
autosave <local@autosave>
2025-11-26 18:11:11 -0500

    auto-save 18:11:11

--- Personalized_Refusal_VLM/scripts/run_idefics3.sh
+++ Personalized_Refusal_VLM/scripts/run_idefics3.sh
@@ -17,26 +17,26 @@ echo "🚀 Step 1: Extracting activations..."
 # python -m experiments.get_activations_inst --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
 
 echo "🧠 Step 2: Training steering vector model..."
-# python -m experiments.train_steering_vector --model_name $model_name --start_layer 0 --end_layer $num_layers
-# for ((layer=0; layer<num_layers; layer+=step)); do
-#     end_layer=$((layer + step))
+python -m experiments.train_steering_vector --model_name $model_name --start_layer 0 --end_layer $num_layers
+for ((layer=0; layer<num_layers; layer+=step)); do
+    end_layer=$((layer + step))
 
-#     if [ $end_layer -gt $num_layers ]; then
-#         end_layer=$num_layers
-#     fi
+    if [ $end_layer -gt $num_layers ]; then
+        end_layer=$num_layers
+    fi
 
-#     echo "🔄 Training layers $layer → $end_layer ..."
+    echo "🔄 Training layers $layer → $end_layer ..."
     
-#     python -m experiments.train_steering_vector \
-#         --model_name $model_name \
-#         --subject "biology" \
-#         --start_layer $layer \
-#         --end_layer $end_layer & # ← 并行运行
-# done
-# wait
+    python -m experiments.train_steering_vector \
+        --model_name $model_name \
+        --subject "biology" \
+        --start_layer $layer \
+        --end_layer $end_layer & # ← 并行运行
+done
+wait
 
-# echo "🎯 Step 3: Inference activations with steering vectors applied..."
-# python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --model_name $model_name
+echo "🎯 Step 3: Inference activations with steering vectors applied..."
+python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --model_name $model_name
 
 echo "🎉 Step 4: Generating responses with steering vectors applied..."
 python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train --inter_start_layer $inter_start_layer \
