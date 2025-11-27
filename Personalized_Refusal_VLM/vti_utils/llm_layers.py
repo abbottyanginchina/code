5244370ec8d4e21f5920c3e856219db915d3a649
autosave <local@autosave>
2025-11-26 22:46:45 -0500

    auto-save 22:46:45

--- Personalized_Refusal_VLM/vti_utils/llm_layers.py
+++ Personalized_Refusal_VLM/vti_utils/llm_layers.py
@@ -14,7 +14,7 @@ class VTILayer(nn.Module):
         super(VTILayer, self).__init__()
         self.vti_direction = vti_direction
         self.lam = lam
-        # self.V_dict = V_dict
+        self.V_dict = V_dict
 
     def forward(self, x):
         if self.vti_direction is not None:
