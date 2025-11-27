fcf0d07f203df795c92d4f6c5e2d2200986f8ffc
autosave <local@autosave>
2025-11-27 18:45:41 -0500

    auto-save 18:45:41

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -624,6 +624,7 @@ def get_activations_inst(cfg, model, inputs_text, image, processor, system_promp
                 elif 'llava-' in cfg.model_name.lower():
                     assistant_token_id = tokenizer("Sorry").input_ids[1]
                     assistant_positions = (input_ids == assistant_token_id).nonzero(as_tuple=True)[0]
+                    import pdb; pdb.set_trace()
                     assistant_start = assistant_positions[-1].item()
                     device = next(model.parameters()).device
                     inputs = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in inputs.items()}
