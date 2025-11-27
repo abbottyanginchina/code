d49bf42a2d4e94114586a9e22d45b78642bb73e4
autosave <local@autosave>
2025-11-27 15:01:02 -0500

    auto-save 15:01:02

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -601,7 +601,7 @@ def get_activations_inst(cfg, model, inputs_text, image, processor, system_promp
                 tokenizer = processor.tokenizer
                 
                 if 'idefics3-' in cfg.model_name.lower():
-                    assistant_token_id = tokenizer("Assistant:").input_ids[1]                
+                    assistant_token_id = tokenizer("Sorry").input_ids[1]                
 
                     assistant_positions = (input_ids == assistant_token_id).nonzero(as_tuple=True)[0]  
                     import pdb; pdb.set_trace() 
