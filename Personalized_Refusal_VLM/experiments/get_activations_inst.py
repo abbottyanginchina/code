1c6244b4a46b7fde3522955b86f4daf658681c2a
autosave <local@autosave>
2025-11-26 19:12:39 -0500

    auto-save 19:12:39

--- Personalized_Refusal_VLM/experiments/get_activations_inst.py
+++ Personalized_Refusal_VLM/experiments/get_activations_inst.py
@@ -159,6 +159,8 @@ def eval_model(cfg):
         with_sys_out_train_activations = process(get_activations_inst(cfg, model, with_sys_out_train_text, out_train_images, processor, system_prompt=True))
         without_sys_in_train_activations = process(get_activations(model, without_sys_in_train_text, in_train_images, processor, system_prompt=False))
         without_sys_out_train_activations = process(get_activations(model, without_sys_out_train_text, out_train_images, processor, system_prompt=False))
+        in_test_activations = process(get_activations(model, in_test_text, in_test_images, processor, system_prompt=False))
+        out_test_activations = process(get_activations(model, out_test_text, out_test_images, processor, system_prompt=False))
 
         # 1. 加 system prompt 的 others（对应 h_c(Image_{others} + system_prompt)）
         with_sys_image_others_activations = process(
@@ -168,9 +170,6 @@ def eval_model(cfg):
         without_sys_image_biology_activations = process(
             get_activations(model, [""] * len(in_train_images), in_train_images, processor, system_prompt=False)
         )
-
-        in_test_activations = process(get_activations(model, in_test_text, in_test_images, processor, system_prompt=False))
-        out_test_activations = process(get_activations(model, out_test_text, out_test_images, processor, system_prompt=False))
         image_in_test_activations = process(
             get_activations(model, [""] * len(in_test_images), in_test_images, processor, system_prompt=False)
         )
