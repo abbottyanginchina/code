407450b45b136c8e8113ddc07cb190e69b450fbe
autosave <local@autosave>
2025-11-27 01:59:43 -0500

    auto-save 01:59:43

--- Personalized_Refusal_VLM/experiments/generation.py
+++ Personalized_Refusal_VLM/experiments/generation.py
@@ -168,15 +168,15 @@ def eval_model(args):
     blip_max_layer = 33
     max_layer = llava_max_layer if 'llava-1.5' in model_path.lower() else qwen_max_layer if 'qwen' in model_path.lower() else blip_max_layer
 
-    for layer in range(1, args.max_layer): # Must start from 1
-        path1 = f"{base_path}/steering_vec_nonbiology_refusal_layer{layer}_{cfg.model_name}.pt"
-        path2 = f"{base_path}/steering_vec_biology_layer{layer}_{cfg.model_name}.pt"
-        vec1 = torch.load(path1, weights_only=False)
-        vec2 = torch.load(path2, weights_only=False)
-        steering_list1.append(vec1)
-        steering_list2.append(vec2)
-    refusal_all = torch.stack(steering_list1, dim=1)
-    biology_all = torch.stack(steering_list2, dim=1)
+    # for layer in range(1, args.max_layer): # Must start from 1
+    #     path1 = f"{base_path}/steering_vec_nonbiology_refusal_layer{layer}_{cfg.model_name}.pt"
+    #     path2 = f"{base_path}/steering_vec_biology_layer{layer}_{cfg.model_name}.pt"
+    #     vec1 = torch.load(path1, weights_only=False)
+    #     vec2 = torch.load(path2, weights_only=False)
+    #     steering_list1.append(vec1)
+    #     steering_list2.append(vec2)
+    # refusal_all = torch.stack(steering_list1, dim=1)
+    # biology_all = torch.stack(steering_list2, dim=1)
     
     # layer = 25
     
