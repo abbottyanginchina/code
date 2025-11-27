57c5896e6ae06376bfe1440325b4b05f61bc34b0
autosave <local@autosave>
2025-11-27 15:14:59 -0500

    auto-save 15:14:59

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -605,7 +605,7 @@ def get_activations_inst(cfg, model, inputs_text, image, processor, system_promp
 
                     assistant_positions = (input_ids == assistant_token_id).nonzero(as_tuple=True)[0]  
                     assistant_start = assistant_positions[-1].item() + 1 
-                    import pdb; pdb.set_trace()
+                    # import pdb; pdb.set_trace()
 
                     device = next(model.parameters()).device
                     inputs = {k: (v.to(device) if isinstance(v, torch.Tensor) else v) for k, v in inputs.items()}
