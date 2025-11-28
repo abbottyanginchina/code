12e23f59c95ce3b59d3928937eae9e42ce686dc2
autosave <local@autosave>
2025-11-27 21:41:01 -0500

    auto-save 21:41:01

--- Personalized_Refusal_VLM/scripts/run_Qwen2.5-VL-7B-Instruct.sh
+++ Personalized_Refusal_VLM/scripts/run_Qwen2.5-VL-7B-Instruct.sh
@@ -15,7 +15,7 @@ dataset="ScienceQA"
 # echo "🚀 Step 1: Extracting activations..."
 python -m experiments.get_activations --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset
 
-echo "🧠 Step 2: Training steering vector model..."
+# echo "🧠 Step 2: Training steering vector model..."
 # # python -m experiments.train_steering_vector --model_name $model_name --start_layer 0 --end_layer 10 &
 # # python -m experiments.train_steering_vector --model_name $model_name --start_layer 10 --end_layer 20 &
 # python -m experiments.train_steering_vector --model_name $model_name --start_layer 20 --end_layer 33
