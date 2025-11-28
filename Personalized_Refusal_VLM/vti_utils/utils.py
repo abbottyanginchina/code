f2e998b051489f25d45f6e73644f2a7fc9c58780
autosave <local@autosave>
2025-11-27 22:50:53 -0500

    auto-save 22:50:53

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -735,7 +735,7 @@ def get_activations_blip_inst(cfg, model, inputs_text, image, processor, system_
             for style_id in range(len(text_list)):
                 text_query = text_list[style_id]
                 if system_prompt:
-                    text_query = system_prompt + 'USER:'+ text_query +'\nASSISTANT: Sorry'
+                    text_query = system_prompt + ' USER:'+ text_query +'\nASSISTANT: Sorry'
 
                 inputs = processor(
                     images=image[example_id],
@@ -789,7 +789,7 @@ def get_activations_blip(model, inputs_text, image, processor, system_prompt=Fal
             for style_id in range(len(text_list)):
                 text_query = text_list[style_id]
                 if system_prompt:
-                    text_query = system_prompt + 'USER:'+ text_query +'\nASSISTANT:'
+                    text_query = system_prompt + ' USER:'+ text_query +'\nASSISTANT:'
                 else system_prompt == False:
                     text_query = ' USER:'+ text_query +'\nASSISTANT:'
 
