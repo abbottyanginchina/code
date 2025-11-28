e2a37ce3cdb7e0b1d97b0296e65f9f7368af6056
autosave <local@autosave>
2025-11-27 19:00:26 -0500

    auto-save 19:00:26

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -623,7 +623,7 @@ def get_activations_inst(cfg, model, inputs_text, image, processor, system_promp
 
                 elif 'llava-' in cfg.model_name.lower():
                     # assistant_token_id = tokenizer("ASSISTANT").input_ids[1]
-                    # assistant_positions = (input_ids == assistant_token_id).nonzero(as_tuple=True)[0]
+                    # assistant_positions = (input_ids == assistant_token_id).nonzero(as_tuple=True)[0] #position:-7
                     import pdb; pdb.set_trace()
                     assistant_start = assistant_positions[-1].item()
                     device = next(model.parameters()).device
