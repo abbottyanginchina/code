d7b5a6772b8b894f3c44612605b893a1c13c8fe3
autosave <local@autosave>
2025-12-02 18:38:09 -0500

    auto-save 18:38:09

--- Personalized_Refusal_VLM/baselines/lora.py
+++ Personalized_Refusal_VLM/baselines/lora.py
@@ -51,7 +51,6 @@ data = load_dataset("json", data_files=train_json)['train']
 def preprocess(example):
     conv = example["conversations"]
     img_path = example["image"]
-    import pdb; pdb.set_trace()
 
     # Build chat template
     prompt = processor.apply_chat_template(
