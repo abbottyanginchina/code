c2a0eb774f7add4707d18964490d939726e15df2
autosave <local@autosave>
2025-12-01 19:19:47 -0500

    auto-save 19:19:47

--- Personalized_Refusal_VLM/baslines/lora.py
+++ Personalized_Refusal_VLM/baslines/lora.py
@@ -1,8 +1,54 @@
 import mmengine
+import argparse
 from vti_utils.utils import get_all_datasets
 
+def generate_answer(cfg):
+    original_data = get_all_datasets(cfg)
 
-original_data = get_all_datasets(cfg)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get Activations")
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="llava",
+        help="Name of the model to use",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="/gpuhome/jmy5701/gpu/models",
+        help="Path to the pretrained models",
+    )
+    parser.add_argument(
+        "--num_test",
+        type=int,
+        default=200,
+        help="Number of test samples to evaluate",
+    )
+    parser.add_argument(
+        "--num_train",
+        type=int,
+        default=200,
+        help="Number of training samples to evaluate",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        help="Name of the dataset to use",
+    )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default="/gpuhome/jmy5701/gpu/data",
+        help="Path to the pretrained models",
+    )
+    parser.add_argument(
+        "--subject",
+        type=str,
+        default="biology",
+        help="Subject to use",
+    )
+    return parser.parse_args()
 
 if __name__ == '__main__':
     config_path = f'configs/cfgs.yaml'
