cab457a1fc3bf0e8f52f950fdb4d0575856337da
autosave <local@autosave>
2025-12-02 15:46:59 -0500

    auto-save 15:46:59

--- Personalized_Refusal_VLM/baselines/eval_lora.py
+++ Personalized_Refusal_VLM/baselines/eval_lora.py
@@ -39,7 +39,7 @@ def chat_VLM(text, img):
     return response.choices[0].message.content
 def generate_answer(cfg):
     response = chat_VLM(text, img)
-        import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     # original_data = get_all_datasets(cfg)
     without_sys_in_train_text = original_data["without_sys_in_train_text"]
     in_train_images = original_data["in_train_images"]
