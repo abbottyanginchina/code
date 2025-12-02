8fd9b33c81956ba0d2d87a5f89ad762b78adf3bc
autosave <local@autosave>
2025-12-02 13:39:08 -0500

    auto-save 13:39:08

--- Personalized_Refusal_VLM/baselines/lora.py
+++ Personalized_Refusal_VLM/baselines/lora.py
@@ -5,6 +5,7 @@ from vti_utils.utils import get_all_datasets
 def generate_answer(cfg):
     original_data = get_all_datasets(cfg)
     import pdb; pdb.set_trace()
+    print("Finished generating data")
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Lora baseline...")
