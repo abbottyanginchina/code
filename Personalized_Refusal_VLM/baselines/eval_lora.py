b85d71de655b6dff99a8b9ebc8fd32f8bbc6cabf
autosave <local@autosave>
2025-12-02 15:49:18 -0500

    auto-save 15:49:18

--- Personalized_Refusal_VLM/baselines/eval_lora.py
+++ Personalized_Refusal_VLM/baselines/eval_lora.py
@@ -15,6 +15,9 @@ def pil_to_b64(img: 'PIL.Image.Image'):
     img.save(buffer, format="PNG")
     return base64.b64encode(buffer.getvalue()).decode()
 def chat_VLM(text, img=None):
+    img = Image.open("../jiaxi.jpy")
+    img = pil_to_b64(img)
+
     response = client.chat.completions.create(
         model="Qwen/Qwen3-VL-32B-Instruct",
         messages=[
