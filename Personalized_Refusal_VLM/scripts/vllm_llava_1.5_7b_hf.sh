70bda749a01d2c6e56b917e29bc02a47f36c7885
autosave <local@autosave>
2025-12-02 17:15:10 -0500

    auto-save 17:15:10

--- Personalized_Refusal_VLM/scripts/vllm_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/vllm_llava_1.5_7b_hf.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=5
 
 python -m vllm.entrypoints.api_server \
     --model /gpuhome/jmy5701/gpu/models/llava-1.5-7b-hf \
-    # --vision-language
+    --served-model-name llava-1.5-7b-hf \
