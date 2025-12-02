30051d39808eabec6911a057b64963e381b68366
autosave <local@autosave>
2025-12-02 13:38:23 -0500

    auto-save 13:38:23

--- Personalized_Refusal_VLM/baselines/lora.py
+++ Personalized_Refusal_VLM/baselines/lora.py
@@ -52,6 +52,7 @@ def parse_args():
     return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
     config_path = f'configs/cfgs.yaml'
     cfg = mmengine.Config.fromfile(config_path)
 
