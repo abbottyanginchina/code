c25f76224ff3dcda957f612a063666b9d30be264
autosave <local@autosave>
2025-12-02 17:25:31 -0500

    auto-save 17:25:31

--- Personalized_Refusal_VLM/baselines/eval_lora.py
+++ Personalized_Refusal_VLM/baselines/eval_lora.py
@@ -140,7 +140,7 @@ def parse_args():
     parser.add_argument(
         "--data_path",
         type=str,
-        default="/gpuhome/jmy5701/gpu/data",
+        default="/gpuhome/jmy5701/gpu/code",
         help="Path to the pretrained models",
     )
     parser.add_argument(
