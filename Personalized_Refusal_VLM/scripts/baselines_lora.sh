45f01171f7563dd8df7a5ca500affb9565770e26
autosave <local@autosave>
2025-12-03 18:30:25 -0500

    auto-save 18:30:25

--- Personalized_Refusal_VLM/scripts/baselines_lora.sh
+++ Personalized_Refusal_VLM/scripts/baselines_lora.sh
@@ -9,6 +9,4 @@ dataset="ScienceQA"
 subject="biology"
 
 # python -m baselines.data_preperation
-
-
 python -m baselines.lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
