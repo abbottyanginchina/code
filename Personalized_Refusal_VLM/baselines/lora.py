53c836d06b800db7449e92b661af12fb14fa7723
autosave <local@autosave>
2025-12-03 15:13:32 -0500

    auto-save 15:13:32

--- Personalized_Refusal_VLM/baselines/lora.py
+++ Personalized_Refusal_VLM/baselines/lora.py
@@ -265,6 +265,26 @@ def inference():
         print(ouptut_text)
 
 if __name__ == "__main__":
+    args = parse_args()
+    config_path = f'configs/cfgs.yaml'
+    cfg = mmengine.Config.fromfile(config_path)
+
+    if args.model_name is not None:
+        cfg.model_name = args.model_name
+    if args.model_path is not None:
+        cfg.model_path = args.model_path
+    if args.num_test is not None:
+        cfg.num_test = args.num_test
+    if args.num_train is not None:  
+        cfg.num_train = args.num_train
+    if args.dataset is not None:
+        cfg.data.dataset_name = args.dataset
+    if args.data_path is not None:
+        cfg.data.path = args.data_path
+    if args.subject is not None:
+        cfg.data.subject = args.subject
+
+
     # train()
 
     inference()
