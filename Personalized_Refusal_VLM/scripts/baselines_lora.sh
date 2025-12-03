06abdafedaaad0431b0ad4ff4b6ae5269201d4fc
autosave <local@autosave>
2025-12-03 18:22:39 -0500

    auto-save 18:22:39

--- Personalized_Refusal_VLM/scripts/baselines_lora.sh
+++ Personalized_Refusal_VLM/scripts/baselines_lora.sh
@@ -5,9 +5,6 @@ export CUDA_VISIBLE_DEVICES=5
 num_train=200
 num_test=200
 model_name="llava-1.5-7b-hf"
-inter_start_layer=12
-inter_end_layer=32
-alpha_text=1.3
 dataset="ScienceQA"
 subject="biology"
 
