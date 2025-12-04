60a0593c548ee505f8557f35166d1d6a04bc066c
autosave <local@autosave>
2025-12-03 19:19:45 -0500

    auto-save 19:19:45

--- Personalized_Refusal_VLM/scripts/baselines_lora.sh
+++ Personalized_Refusal_VLM/scripts/baselines_lora.sh
@@ -8,7 +8,7 @@ model_name="llava-1.5-7b-hf"
 dataset="ScienceQA"
 subject="biology"
 
-python -m baselines.data_preperation --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
+# python -m baselines.data_preperation --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
 python -m baselines.lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
 
 # subject="physics"
