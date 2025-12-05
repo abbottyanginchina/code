71bf9792bbabc4fe1b546e37c11489041a6976a5
autosave <local@autosave>
2025-12-05 13:29:09 -0500

    auto-save 13:29:09

--- Personalized_Refusal_VLM/experiments/generation.py
+++ Personalized_Refusal_VLM/experiments/generation.py
@@ -499,4 +499,6 @@ if __name__ == "__main__":
         cfg.data.subject = args.subject
 
     set_seed(cfg.seed)
+
+    output_dir = f"{cfg.output_dir}/output_{cfg.model_name}_{cfg.data.dataset_name}"
     eval_model(cfg)
