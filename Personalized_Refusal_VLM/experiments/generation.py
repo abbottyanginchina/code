ac442171493cd75fa559c6e96bd1f7d08a3dc6b5
autosave <local@autosave>
2025-12-04 23:32:52 -0500

    auto-save 23:32:52

--- Personalized_Refusal_VLM/experiments/generation.py
+++ Personalized_Refusal_VLM/experiments/generation.py
@@ -178,6 +178,7 @@ def eval_model(args):
     refusal_all = torch.stack(steering_list1, dim=1)
     biology_all = torch.stack(steering_list2, dim=1)
     
+    import pdb; pdb.set_trace()
     # layer = 25
     
     # 这里最大层数是前面减1，因为第零层不取
