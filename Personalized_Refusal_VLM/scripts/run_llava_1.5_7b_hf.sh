d839f1dab17abfd70a3728b2cba3f4a7f0941c0a
autosave <local@autosave>
2025-12-05 14:41:36 -0500

    auto-save 14:41:36

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -23,22 +23,22 @@ python -m experiments.get_activations_inst --model_name $model_name --num_test $
 # wait
 
 
-for ((layer=0; layer<num_layers; layer+=step)); do
-    end_layer=$((layer + step))
+# for ((layer=0; layer<num_layers; layer+=step)); do
+#     end_layer=$((layer + step))
 
-    if [ $end_layer -gt $num_layers ]; then
-        end_layer=$num_layers
-    fi
+#     if [ $end_layer -gt $num_layers ]; then
+#         end_layer=$num_layers
+#     fi
 
-    echo "🔄 Training layers $layer → $end_layer ..."
+#     echo "🔄 Training layers $layer → $end_layer ..."
     
-    python -m experiments.train_steering_vector \
-        --model_name $model_name \
-        --subject $subject \
-        --start_layer $layer \
-        --end_layer $end_layer & # ← 并行运行
-done
-wait
+#     python -m experiments.train_steering_vector \
+#         --model_name $model_name \
+#         --subject $subject \
+#         --start_layer $layer \
+#         --end_layer $end_layer & # ← 并行运行
+# done
+# wait
 
 echo "✅ Step 3: Inference activations with steering vectors applied..."
 python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers
