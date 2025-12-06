f1020e0ea211a7dd53a2fb811f36ee37835a28f9
autosave <local@autosave>
2025-12-06 14:11:32 -0500

    auto-save 14:11:32

--- Personalized_Refusal_VLM/vti_utils/utils.py
+++ Personalized_Refusal_VLM/vti_utils/utils.py
@@ -615,7 +615,6 @@ def get_activations_inst(cfg, model, inputs_text, image, processor, system_promp
                             return_dict=True
                         )
                         h = h.hidden_states
-                        import pdb; pdb.set_trace()
 
                     embedding_token = []
                     for layer in range(len(h)):
