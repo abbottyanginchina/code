b672018a334f3c3a877fb5f0008b36a767bdf1e7
autosave <local@autosave>
2025-12-06 15:27:59 -0500

    auto-save 15:27:59

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -55,7 +55,7 @@ python -m experiments.generation --model_name $model_name --num_test $num_test -
 
 }
 
-echo "Start ScienceQA..."
+
 dataset="ScienceQA"
 subjects=("biology" "chemistry" "physics")  # 根据需要修改这里的 subjects
 for subject in "${subjects[@]}"; do
