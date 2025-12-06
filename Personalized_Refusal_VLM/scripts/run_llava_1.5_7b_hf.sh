4e8e90194b73d7eb5476461505bfa48ba6bf6536
autosave <local@autosave>
2025-12-06 17:23:50 -0500

    auto-save 17:23:50

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -17,33 +17,33 @@ process_subject() {
     dataset=$1
     subject=$2
     
-    # echo "🎉 Dataset $dataset, subject $subject started!"
-    # echo "🚀 Step 1: Extracting activations..."
-    # python -m experiments.get_activations_inst --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
+    echo "🎉 Dataset $dataset, subject $subject started!"
+    echo "🚀 Step 1: Extracting activations..."
+    python -m experiments.get_activations_inst --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
 
-    # echo "🧠 Step 2: Training steering vector model..."
+    echo "🧠 Step 2: Training steering vector model..."
     # python -m experiments.train_steering_vector --model_name $model_name --start_layer 12 --end_layer 19 &
     # python -m experiments.train_steering_vector --model_name $model_name --start_layer 19 --end_layer 26 &
     # python -m experiments.train_steering_vector --model_name $model_name --start_layer 26 --end_layer 33 &
     # wait
 
-    # for ((layer=0; layer<num_layers; layer+=step)); do
-    #     end_layer=$((layer + step))
+    for ((layer=0; layer<num_layers; layer+=step)); do
+        end_layer=$((layer + step))
 
-    #     if [ $end_layer -gt $num_layers ]; then
-    #         end_layer=$num_layers
-    #     fi
+        if [ $end_layer -gt $num_layers ]; then
+            end_layer=$num_layers
+        fi
 
-    #     echo "🔄 Training layers $layer → $end_layer ..."
+        echo "🔄 Training layers $layer → $end_layer ..."
         
-    #     python -m experiments.train_steering_vector \
-    #         --model_name $model_name \
-    #         --subject $subject \
-    #         --dataset $dataset \
-    #         --start_layer $layer \
-    #         --end_layer $end_layer & # ← 并行运行
-    # done
-    # wait
+        python -m experiments.train_steering_vector \
+            --model_name $model_name \
+            --subject $subject \
+            --dataset $dataset \
+            --start_layer $layer \
+            --end_layer $end_layer & # ← 并行运行
+    done
+    wait
 
     echo "✅ Step 3: Inference activations with steering vectors applied..."
     python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --dataset $dataset
@@ -59,11 +59,11 @@ process_subject() {
 }
 
 
-# dataset="ScienceQA"
-# subjects=("biology" "geography" "physics")  # 根据需要修改这里的 subjects
-# for subject in "${subjects[@]}"; do
-#     process_subject "$dataset" "$subject"
-# done
+dataset="ScienceQA"
+subjects=("biology" "geography" "physics")  # 根据需要修改这里的 subjects
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject"
+done
 
 dataset="MMMU"
 subjects=("Math" "Geography" "Art_Theory")  # 根据需要修改这里的 subjects
