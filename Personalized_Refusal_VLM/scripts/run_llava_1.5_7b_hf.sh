74c4bd7153eba77a5d5b95982b2673194b388008
autosave <local@autosave>
2025-12-06 15:29:03 -0500

    auto-save 15:29:03

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -17,6 +17,7 @@ process_subject() {
     dataset=$1
     subject=$2
     
+    echo "🎉 Dataset $dataset, subject $subject started!"
     echo "🚀 Step 1: Extracting activations..."
     python -m experiments.get_activations_inst --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
 
