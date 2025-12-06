90e7924a25afaff4446be36b30d7a7ed5eb14fac
autosave <local@autosave>
2025-12-06 17:42:40 -0500

    auto-save 17:42:40

--- Personalized_Refusal_VLM/baselines/data_preperation.py
+++ Personalized_Refusal_VLM/baselines/data_preperation.py
@@ -150,7 +150,7 @@ def parse_args():
     parser.add_argument(
         "--model_path",
         type=str,
-        default="/gpuhome/jmy5701/gpu/models",
+        default="../../models",
         help="Path to the pretrained models",
     )
     parser.add_argument(
@@ -173,7 +173,7 @@ def parse_args():
     parser.add_argument(
         "--data_path",
         type=str,
-        default="/gpuhome/jmy5701/gpu/data",
+        default="../../data",
         help="Path to the pretrained models",
     )
     parser.add_argument(
