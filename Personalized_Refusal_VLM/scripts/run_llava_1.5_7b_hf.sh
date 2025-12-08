da4dd9e0d2d0a46a308daa7b0b9439cfb238b9b1
autosave <local@autosave>
2025-12-07 20:34:42 -0500

    auto-save 20:34:42

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_7b_hf.sh
@@ -43,9 +43,9 @@ process_subject() {
     done
     wait
 
-    echo "✅ Step 3: Inference activations with steering vectors applied..."
+    # echo "✅ Step 3: Inference activations with steering vectors applied..."
     # python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --dataset $dataset --model_name $model_name
-    echo "✅ All layer groups finished!"
+    # echo "✅ All layer groups finished!"
 
     echo "🎯 Step 4: Generating responses with steering vectors applied..."
     python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train \
