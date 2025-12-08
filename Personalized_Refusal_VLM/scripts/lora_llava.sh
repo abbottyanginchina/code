4fd9d3cc219c12cd3a5a5bf349e85eaa76cd6651
autosave <local@autosave>
2025-12-08 14:43:34 -0500

    auto-save 14:43:34

--- Personalized_Refusal_VLM/scripts/lora_llava.sh
+++ Personalized_Refusal_VLM/scripts/lora_llava.sh
@@ -15,7 +15,7 @@ process() {
     echo "🎉 Dataset $dataset, subject $subject started!"
     python -m baselines.data_preperation --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
     wait
-    python -m baselines.lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
+    python -m baselines.llava_lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
     wait
     echo "🎉 Dataset $dataset, subject $subject completed!"
 }
