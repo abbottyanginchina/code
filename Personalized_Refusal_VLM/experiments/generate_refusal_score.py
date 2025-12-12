a9342e46368913c463c1f15930a50d8829949a67
autosave <local@autosave>
2025-12-11 21:09:49 -0500

    auto-save 21:09:49

--- Personalized_Refusal_VLM/experiments/generate_refusal_score.py
+++ Personalized_Refusal_VLM/experiments/generate_refusal_score.py
@@ -198,10 +198,6 @@ def eval_model(args, output_dir):
 
     if not os.path.exists(f"../output_{cfg.model_name}_{cfg.data.dataset_name}/results/"):
         os.makedirs(f"../output_{cfg.model_name}_{cfg.data.dataset_name}/results/")
-  
-    add_multiple_layers(model, torch.stack([refusal_all[img_id]],dim=1).cuda(), alpha = [cfg.alpha_text], layer_indices = target_layers, cfg = cfg)
-    out_refusal_scores = get_generation_refusal_scores(cfg, model, processor, processor.tokenizer, out_test_text, out_test_images)
-    remove_multiple_layers(model, layer_indices = target_layers, cfg = cfg)
 
     # 生成拒绝测试集
     # answers_file = f"{output_dir}/results/nonbiology_answer_{cfg.model_name}.jsonl"
