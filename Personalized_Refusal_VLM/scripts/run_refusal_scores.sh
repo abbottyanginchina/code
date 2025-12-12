1ded520f14ce199e96866a8b68a802d26d8bbf17
autosave <local@autosave>
2025-12-11 21:08:10 -0500

    auto-save 21:08:10

--- Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
+++ Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
@@ -1,2 +1,5 @@
 set -e
 export CUDA_VISIBLE_DEVICES=6
+
+python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train \
+        --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
