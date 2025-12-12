22c4b2e8a469165f28bc27baba825a6f2136033e
autosave <local@autosave>
2025-12-11 20:49:45 -0500

    auto-save 20:49:45

--- Personalized_Refusal_VLM/vti_utils/get_refusal_score.py
+++ Personalized_Refusal_VLM/vti_utils/get_refusal_score.py
@@ -134,7 +134,7 @@ def get_generation_refusal_scores(cfg, model, processor, tokenizer, test_text, t
 
 def filter_data(cfg, model, processor, tokenizer, with_sys_out_train_text, without_sys_out_train_text, out_train_images):
 
-    score_list = []
+    score_with_list, score_no_list, gap_list = [], [], []
     for i in tqdm(range(0, len(with_sys_out_train_text)), desc="Filtering samples", total=len(with_sys_out_train_text)):
         image = load_image(out_train_images[i])
     
