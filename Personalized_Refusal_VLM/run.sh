7af740a9c10dc9e09b2d47a5b5250ae9559ad0e7
autosave <local@autosave>
2025-12-12 13:56:59 -0500

    auto-save 13:56:59

--- Personalized_Refusal_VLM/run.sh
+++ Personalized_Refusal_VLM/run.sh
@@ -1,8 +1,8 @@
 echo "Running our method..."
 bash scripts/ours.sh
 
-echo "Running baseline method..."
-bash scripts/persona_baseline.sh
+# echo "Running baseline method..."
+# bash scripts/persona_baseline.sh
 
 echo "Running refusal score..."
 bash scripts/refusal_score.sh
