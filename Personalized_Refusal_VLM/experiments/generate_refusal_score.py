79dc841a61dd197ebc8606af3e8530b124df67db
autosave <local@autosave>
2025-12-11 21:20:43 -0500

    auto-save 21:20:43

--- Personalized_Refusal_VLM/experiments/generate_refusal_score.py
+++ Personalized_Refusal_VLM/experiments/generate_refusal_score.py
@@ -204,7 +204,7 @@ def eval_model(args, output_dir):
     # os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     # ans_file = open(answers_file, "w")
 
-    for img_id in range(len(out_test_images)):
+    for img_id in tqdm(range(len(out_test_images)), desc="Generating refusal scores", total=len(out_test_images)):
         raw_image = load_image(out_test_images[img_id])
         question = out_test_text[img_id]
         add_multiple_layers(model, torch.stack([refusal_all[img_id]],dim=1).cuda(), alpha = [cfg.alpha_text], layer_indices = target_layers, cfg = cfg)
