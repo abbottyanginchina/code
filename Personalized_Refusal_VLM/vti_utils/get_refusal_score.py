c415e427d6145cb78d6be22c4c7b68a31914a9e0
autosave <local@autosave>
2025-12-11 21:10:31 -0500

    auto-save 21:10:31

--- Personalized_Refusal_VLM/vti_utils/get_refusal_score.py
+++ Personalized_Refusal_VLM/vti_utils/get_refusal_score.py
@@ -109,7 +109,6 @@ def get_refusal_scores_llava(
 
 def get_generation_refusal_scores(cfg, model, processor, tokenizer, test_text, test_images):
     scores = []
-    import pdb; pdb.set_trace()
     for i in tqdm(range(0, len(test_text)), desc="Getting generation refusal scores", total=len(test_text)):
         image = load_image(test_images[i])
         conversation = [
