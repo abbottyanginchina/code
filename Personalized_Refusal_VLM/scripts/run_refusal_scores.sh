717c349400a7df6af9b1b3d2eed32c7c0d69b2fd
autosave <local@autosave>
2025-12-11 21:08:21 -0500

    auto-save 21:08:21

--- Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
+++ Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
@@ -1,5 +1,14 @@
 set -e
 export CUDA_VISIBLE_DEVICES=6
 
+model_name="llava-1.5-7b-hf"
+num_test=200
+num_train=200
+inter_start_layer=12
+inter_end_layer=32
+alpha_text=1.7
+dataset="ScienceQA"
+subject="biology"
+
 python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train \
         --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
