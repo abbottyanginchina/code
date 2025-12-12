947c4504f72bb2bcf9dd8d4cbb159ae9866de28d
autosave <local@autosave>
2025-12-11 21:08:40 -0500

    auto-save 21:08:40

--- Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
+++ Personalized_Refusal_VLM/scripts/run_refusal_scores.sh
@@ -12,3 +12,5 @@ subject="biology"
 
 python -m experiments.generate_refusal_score --model_name $model_name --num_test $num_test --num_train $num_train \
         --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
+
+echo "✅ All finished!"
