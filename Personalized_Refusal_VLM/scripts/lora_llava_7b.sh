744b134358b9f328dec8743ef7021861de90d39a
autosave <local@autosave>
2025-12-17 06:39:58 +0800

    auto-save 06:39:58

---
+++ Personalized_Refusal_VLM/scripts/lora_llava_7b.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+export CUDA_VISIBLE_DEVICES=7
+
+num_train=200
+num_test=200
+model_name="llava-1.5-13b-hf"
+dataset="ScienceQA"
+subject="biology"
+
+process() {
+    dataset=$1
+    subject=$2
+    
+    echo "🎉 Dataset $dataset, subject $subject started!"
+    python -m baselines.data_preperation --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
+    wait
+    python -m baselines.llava_lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
+    wait
+    echo "🎉 Dataset $dataset, subject $subject completed!"
+}
+
+# dataset="ScienceQA"
+# subjects=("biology" "geography" "physics")  # 根据需要修改这里的 subjects
+# for subject in "${subjects[@]}"; do
+#     process "$dataset" "$subject"
+# done
+
+dataset="MMMU"
+subjects=("Math" "Geography" "Art_Theory")  # 根据需要修改这里的 subjects
+for subject in "${subjects[@]}"; do
+    process "$dataset" "$subject"
+done
+
+echo "🎉 All steps completed!"
