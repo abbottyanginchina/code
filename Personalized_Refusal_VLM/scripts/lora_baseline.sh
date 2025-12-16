7f76c1e731faca27775045c31e89c4c16b79d2c9
autosave <local@autosave>
2025-12-17 06:42:09 +0800

    auto-save 06:42:09

--- Personalized_Refusal_VLM/scripts/lora_baseline.sh
+++ Personalized_Refusal_VLM/scripts/lora_baseline.sh
@@ -3,12 +3,12 @@ export CUDA_VISIBLE_DEVICES=6
 echo "🎉 Baseline Lora started!"
 
 echo "LLAVA 7B"
-bash lora_llava_7b.sh
+bash ./scripts/lora_llava_7b.sh
 
 echo "LLAVA 13B"
-bash lora_llava_13b.sh
+bash ./scripts/lora_llava_13b.sh
 
 echo "Idefics3"
-bash lora_idefics3.sh
+bash ./scripts/lora_idefics3.sh
 
 echo "🎉 Baseline Lora completed!"
