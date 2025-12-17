a21a9604bee99e6729e53ecb4538aac38eede864
autosave <local@autosave>
2025-12-17 19:40:09 +0800

    auto-save 19:40:09

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
@@ -63,11 +63,11 @@ for subject in "${subjects[@]}"; do
     process_subject "$dataset" "$subject"
 done
 
-dataset="MMMU"
-subjects=("Math" "Geography" "Art_Theory")  
-for subject in "${subjects[@]}"; do
-    process_subject "$dataset" "$subject"
-done
+# dataset="MMMU"
+# subjects=("Math" "Geography" "Art_Theory")  
+# for subject in "${subjects[@]}"; do
+#     process_subject "$dataset" "$subject"
+# done
 
 echo "🎉 All steps completed!"
 
