2ae3d4a84f224e867a1f98a9470646c2cd471780
autosave <local@autosave>
2025-12-17 11:22:18 +0800

    auto-save 11:22:18

--- Personalized_Refusal_VLM/scripts/sys_baseline.sh
+++ Personalized_Refusal_VLM/scripts/sys_baseline.sh
@@ -2,6 +2,7 @@ export CUDA_VISIBLE_DEVICES=5,6
 
 echo "🚀 Step 1: Generating responses for llava-1.5-7b-hf..."
 bash scripts/sys_llava_7b.sh
+
 echo "🚀 Step 2: Generating responses for llava-1.5-13b-hf..."
 bash scripts/sys_llava_13b.sh
 echo "🚀 Step 3: Generating responses for Idefics3-8B-Llama3..."
