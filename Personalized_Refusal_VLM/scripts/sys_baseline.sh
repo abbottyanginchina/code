b7caa0014150ec4814070e4883990ca71c8352ee
autosave <local@autosave>
2025-12-17 11:22:27 +0800

    auto-save 11:22:27

--- Personalized_Refusal_VLM/scripts/sys_baseline.sh
+++ Personalized_Refusal_VLM/scripts/sys_baseline.sh
@@ -5,5 +5,6 @@ bash scripts/sys_llava_7b.sh
 
 echo "🚀 Step 2: Generating responses for llava-1.5-13b-hf..."
 bash scripts/sys_llava_13b.sh
+
 echo "🚀 Step 3: Generating responses for Idefics3-8B-Llama3..."
 bash scripts/sys_idefics3.sh
