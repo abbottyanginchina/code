24a06651acd1013320c08b944c4def1188bd3476
autosave <local@autosave>
2025-12-17 20:37:55 +0800

    auto-save 20:37:55

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
@@ -23,23 +23,23 @@ process_subject() {
 
     # wait
 
-    for ((layer=0; layer<num_layers; layer+=step)); do
-        end_layer=$((layer + step))
+    # for ((layer=0; layer<num_layers; layer+=step)); do
+    #     end_layer=$((layer + step))
 
-        if [ $end_layer -gt $num_layers ]; then
-            end_layer=$num_layers
-        fi
+    #     if [ $end_layer -gt $num_layers ]; then
+    #         end_layer=$num_layers
+    #     fi
 
-        echo "🔄 Training layers $layer → $end_layer ..."
+    #     echo "🔄 Training layers $layer → $end_layer ..."
         
-        python -m experiments.train_steering_vector \
-            --model_name $model_name \
-            --subject $subject \
-            --dataset $dataset \
-            --start_layer $layer \
-            --end_layer $end_layer & # ← 并行运行
-    done
-    wait
+    #     python -m experiments.train_steering_vector \
+    #         --model_name $model_name \
+    #         --subject $subject \
+    #         --dataset $dataset \
+    #         --start_layer $layer \
+    #         --end_layer $end_layer & # ← 并行运行
+    # done
+    # wait
 
     echo "✅ Step 3: Inference activations with steering vectors applied..."
     python -m experiments.inference_activations --start_layer 0 --end_layer $num_layers --subject $subject --model_name $model_name --dataset $dataset
