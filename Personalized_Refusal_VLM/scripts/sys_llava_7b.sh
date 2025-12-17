e75d32ec99f72c3b7a2aa7317b1f671ee994e5c0
autosave <local@autosave>
2025-12-17 11:00:06 +0800

    auto-save 11:00:06

---
+++ Personalized_Refusal_VLM/scripts/sys_llava_7b.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+num_layers=33 # Example number of layers for llava-1.5-7b-hf
+step=7
+num_train=200
+num_test=200
+model_name="instructblip-vicuna-7b"
+inter_start_layer=15
+inter_end_layer=32
+alpha_text=2.5
+
+process_subject() {
+    dataset=$1
+    subject=$2
+    
+    echo "🎉 Dataset $dataset, subject $subject started!"
+
+    echo "🎯 Generating responses..."
+    python -m experiments.prompt_generation --model_name $model_name --num_test $num_test --num_train $num_train \
+        --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
+
+    echo "🎉 Dataset $dataset, subject $subject completed!"
+    echo ""
+}
+
+
+# dataset="ScienceQA"
+# subjects=("biology" "geography" "physics")  
+# for subject in "${subjects[@]}"; do
+#     process_subject "$dataset" "$subject"
+# done
+
+dataset="MMMU"
+subjects=("Math" "Geography" "Art_Theory") 
+# subjects=("Geography" "Art_Theory") 
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject"
+done
+
+echo "🎉 All steps completed!"
+
+
+
