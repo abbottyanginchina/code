d08830da7bb2ea70156dd244b83156e99554aa15
autosave <local@autosave>
2025-12-17 11:06:26 +0800

    auto-save 11:06:26

--- Personalized_Refusal_VLM/experiments/prompt_generation.py
+++ Personalized_Refusal_VLM/experiments/prompt_generation.py
@@ -168,7 +168,7 @@ def eval_model(cfg, output_dir):
     answers_file = f"{output_dir}/results/sysprompt_non{cfg.data.subject}_answer_{cfg.model_name}.jsonl"
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     ans_file = open(answers_file, "w")
-    for img_id in tqdm(range(len(out_test_images))):
+    for img_id in range(len(out_test_images)):
         raw_image = load_image(out_test_images[img_id])
         question = out_test_text[img_id]
 
@@ -242,7 +242,7 @@ def eval_model(cfg, output_dir):
     answers_file = f"{output_dir}/results/sysprompt_{cfg.data.subject}_answer_{cfg.model_name}.jsonl"
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     ans_file = open(answers_file, "w")
-    for img_id in tqdm(range(len(in_test_images))):
+    for img_id in range(len(in_test_images)):
         raw_image = load_image(in_test_images[img_id]) #########
         question = in_test_text[img_id] ##########
         
