094c5178de16162be6c2acda876392795fbcf62d
autosave <local@autosave>
2025-12-22 16:53:18 +0800

    auto-save 16:53:18

--- Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
+++ Personalized_Refusal_VLM/scripts/run_llava_1.5_13b_hf.sh
@@ -47,7 +47,7 @@ process_subject() {
     echo "✅ All layer groups finished!"
 
     echo "🎯 Step 4: Generating responses with steering vectors applied..."
-    # python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train \
+    python -m experiments.generation --model_name $model_name --num_test $num_test --num_train $num_train \
     #     --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject --max_layer $num_layers
 
     echo "🎉 Dataset $dataset, subject $subject completed!"
