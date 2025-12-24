453b74cc82c66b17d0ee201dad1fb7a0916bd7f4
autosave <local@autosave>
2025-12-24 14:58:49 +0800

    auto-save 14:58:49

--- Personalized_Refusal_VLM/scripts/orth_llava_1.5_7b.sh
+++ Personalized_Refusal_VLM/scripts/orth_llava_1.5_7b.sh
@@ -56,8 +56,8 @@ process_subject() {
 
 
 dataset="ScienceQA"
-subjects=("biology" "geography" "physics")  
-# subjects=("geography" "physics") 
+# subjects=("biology" "geography" "physics")  
+subjects=("geography" "physics") 
 for subject in "${subjects[@]}"; do
     process_subject "$dataset" "$subject"
 done
