0598ea95077164fa7118620a3092809b157e470b
autosave <local@autosave>
2025-12-26 21:09:08 +0800

    auto-save 21:09:08

--- Personalized_Refusal_VLM/scripts/lora_idefics3.sh
+++ Personalized_Refusal_VLM/scripts/lora_idefics3.sh
@@ -15,7 +15,7 @@ process() {
     python -m baselines.data_preperation --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
     # wait
     python -m baselines.Idefics3_lora --model_name $model_name --num_test $num_test --num_train $num_train --dataset $dataset --subject $subject
-    wait
+    # wait
     echo "🎉 Dataset $dataset, subject $subject completed!"
 }
 
