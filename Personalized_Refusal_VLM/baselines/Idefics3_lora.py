48bd0c3991e3d26a6a015d3ca28356feaa34c107
autosave <local@autosave>
2025-12-27 10:21:45 +0800

    auto-save 10:21:45

--- Personalized_Refusal_VLM/baselines/Idefics3_lora.py
+++ Personalized_Refusal_VLM/baselines/Idefics3_lora.py
@@ -344,6 +344,7 @@ def parse_args():
     parser.add_argument(
         "--data_path",
         type=str,
+        default="../../data",
         help="Path to the pretrained models",
     )
     parser.add_argument(
