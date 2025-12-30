2ce9156bfd0937d596249ab13f590230ed6a65ff
autosave <local@autosave>
2025-12-30 11:05:47 +0800

    auto-save 11:05:47

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
-# from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
-# import torch
-# from PIL import Image
-# import requests
+# # from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
+# # import torch
+# # from PIL import Image
+# # import requests
 
-# device = "cuda:7" if torch.cuda.is_available() else "cpu"
-# model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b").to(device)
-# processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
+# # device = "cuda:7" if torch.cuda.is_available() else "cpu"
+# # model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b").to(device)
+# # processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
 
-# url = "https://raw.githubusercontent.com/salesforce/LAVIS/main/docs/_static/Confusing-Pictures.jpg"
-# image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-# prompt = "What is unusual about this image?"
-# inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
+# # url = "https://raw.githubusercontent.com/salesforce/LAVIS/main/docs/_static/Confusing-Pictures.jpg"
+# # image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+# # prompt = "What is unusual about this image?"
+# # inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
 
-# outputs = model.generate(
-#         **inputs,
-#         do_sample=False,
-#         num_beams=5,
-#         max_length=256,
-#         min_length=1,
-#         top_p=0.9,
-#         repetition_penalty=1.5,
-#         length_penalty=1.0,
-#         temperature=1,
-# )
-# generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-# print(generated_text)
+# # outputs = model.generate(
+# #         **inputs,
+# #         do_sample=False,
+# #         num_beams=5,
+# #         max_length=256,
+# #         min_length=1,
+# #         top_p=0.9,
+# #         repetition_penalty=1.5,
+# #         length_penalty=1.0,
+# #         temperature=1,
+# # )
+# # generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+# # print(generated_text)
 
-print("hello world****dddd")
+# print("hello world****dddd")
