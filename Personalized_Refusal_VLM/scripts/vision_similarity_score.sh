bc7ef7f68381b83ea96381478a4003fbe2b755b7
autosave <local@autosave>
2025-12-31 11:33:39 +0800

    auto-save 11:33:39

--- Personalized_Refusal_VLM/scripts/vision_similarity_score.sh
+++ Personalized_Refusal_VLM/scripts/vision_similarity_score.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-
-num_layers=33 # Example number of layers for llava-1.5-7b-hf
-step=11
-num_train=200
-num_test=200
+num_layers=33 
 model_name="llava-1.5-7b-hf"
-inter_start_layer=12
-inter_end_layer=32
-alpha_text=1.7
+dataset="ScienceQA"
+subject="biology"
 
 
 python -m experiments.vision_experiments.similarity_score --start_layer 0 --end_layer $num_layers --subject $subject --dataset $dataset --model_name $model_name
