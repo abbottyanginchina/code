3cdf131d63a4727b966350d659d9c0f68f558c20
autosave <local@autosave>
2025-12-31 11:29:39 +0800

    auto-save 11:29:39

--- Personalized_Refusal_VLM/experiments/vision_experiments/similarity_score.py
+++ Personalized_Refusal_VLM/experiments/vision_experiments/similarity_score.py
@@ -2,8 +2,8 @@ import torch
 import argparse
 
 def load_activations(cfg, layer, output_dir):
-    bio_x = torch.load(f"{output_dir}/activations/image_pred_other_layer{layer}_{cfg.model_name}.pt", weights_only=False)[:, layer, :]
-    oth_x = torch.load(f"{output_dir}/activations/image_pred_biology_layer{layer}_{cfg.model_name}.pt", weights_only=False)[:, layer, :]
+    image_pred_other_x = torch.load(f"{output_dir}/activations/image_pred_other_layer{layer}_{cfg.model_name}.pt", weights_only=False)[:, layer, :]
+    image_pred_biology_x = torch.load(f"{output_dir}/activations/image_pred_biology_layer{layer}_{cfg.model_name}.pt", weights_only=False)[:, layer, :]
     
 
-    return bio_x, oth_x, bio_target, oth_target
+    
