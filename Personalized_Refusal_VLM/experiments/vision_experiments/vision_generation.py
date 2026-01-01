6f8c30fdf8f41486db57aa3c9876c0bd4cee139c
autosave <local@autosave>
2026-01-01 13:37:13 +0800

    auto-save 13:37:13

--- Personalized_Refusal_VLM/experiments/vision_experiments/vision_generation.py
+++ Personalized_Refusal_VLM/experiments/vision_experiments/vision_generation.py
@@ -204,7 +204,7 @@ def eval_model(args, output_dir):
     ans_file = open(answers_file, "w")
     for img_id in range(len(out_test_images)):
         raw_image = load_image(out_test_images[img_id])
-        # question = out_test_text[img_id]
+        question = out_test_text[img_id]
         question = "You are a helpful assistant. Please answer the question based on the image."
         add_multiple_layers(model, torch.stack([refusal_all[img_id]],dim=1).cuda(), alpha = [cfg.alpha_text], layer_indices = target_layers, cfg = cfg)
         # add_multiple_layers(model, torch.stack([refusal_vector],dim=1).cuda(), alpha = [cfg.alpha_text], layer_indices = target_layers, cfg=cfg)
