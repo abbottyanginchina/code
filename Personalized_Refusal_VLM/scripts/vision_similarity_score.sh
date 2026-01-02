58d517f60c5d2618b25c744c53a8ecea44342687
autosave <local@autosave>
2026-01-02 22:29:26 +0800

    auto-save 22:29:26

--- Personalized_Refusal_VLM/scripts/vision_similarity_score.sh
+++ Personalized_Refusal_VLM/scripts/vision_similarity_score.sh
@@ -3,8 +3,24 @@ set -e
 
 num_layers=33 
 model_name="llava-1.5-7b-hf"
+
+process_subject() {
+    dataset=$1
+    subject=$2
+    python -m experiments.vision_experiments.similarity_score --start_layer 0 --end_layer $num_layers --subject $subject --dataset $dataset --model_name $model_name
+}
+
 dataset="ScienceQA"
-subject="biology"
+subjects=("biology" "geography" "physics")  
+# subjects=("physics") 
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject"
+done
 
+dataset="MMMU"
+subjects=("Math" "Geography" "Art_Theory") 
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject"
+done
 
-python -m experiments.vision_experiments.similarity_score --start_layer 0 --end_layer $num_layers --subject $subject --dataset $dataset --model_name $model_name
+echo "🎉 All steps completed!"
