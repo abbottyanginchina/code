ae0feb38e4069f60a50f7d7141212292ba952a7c
autosave <local@autosave>
2026-01-16 18:23:10 +0800

    auto-save 18:23:10

---
+++ Personalized_Refusal_VLM/scripts/vision_refusal_score_llava_7b.sh
@@ -0,0 +1,34 @@
+set -e
+export CUDA_VISIBLE_DEVICES=6
+
+model_name="llava-1.5-7b-hf"
+num_test=200
+num_train=200
+inter_start_layer=12
+inter_end_layer=32
+alpha_text=1.7
+dataset="ScienceQA"
+subject="biology"
+
+process_subject() {
+    dataset=$1
+    subject=$2
+    
+    echo "🎉 Dataset $dataset, subject $subject, model $model_name started!"
+    python -m experiments.generate_refusal_score --model_name $model_name --num_test $num_test --num_train $num_train \
+        --inter_start_layer $inter_start_layer --inter_end_layer $inter_end_layer --alpha_text $alpha_text --dataset $dataset --subject $subject
+
+}
+
+dataset="ScienceQA"
+subjects=("biology" "geography" "physics")  
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject" 
+done
+
+dataset="MMMU"
+subjects=("Math" "Geography" "Art_Theory") 
+for subject in "${subjects[@]}"; do
+    process_subject "$dataset" "$subject" 
+done
+echo "✅ All finished!"
