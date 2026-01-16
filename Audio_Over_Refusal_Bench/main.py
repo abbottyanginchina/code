3a4e79ced9bf70fd28e4169ebd7601df3aa0fc67
autosave <local@autosave>
2026-01-17 00:24:04 +0800

    auto-save 00:24:04

--- Audio_Over_Refusal_Bench/main.py
+++ Audio_Over_Refusal_Bench/main.py
@@ -2,6 +2,7 @@ import os
 from datasets import load_dataset
 from utils.extract_words import get_highlighted_text
 from utils.audio_generation import generation_audio 
+from tqdm import tqdm
 
 if __name__ == "__main__":
     # text = "Hello, I am Xiaomi, nice to meet you."
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     if not os.path.exists(os.path.join(data_path, 'audio')):
         os.makedirs(os.path.join(data_path, 'audio'))
 
-    for idx in range(10):
+    for idx in tqdm(range(10), desc="Generating audio", total=len(dataset)):
         prompt = dataset[idx]['prompt']
 
         save_path = os.path.join(data_path, 'audio', f'{idx}.mp3')
