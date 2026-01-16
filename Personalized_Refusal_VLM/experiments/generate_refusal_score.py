959b86a4dd279be0c7a0fcd24a27142a2f4cea12
autosave <local@autosave>
2026-01-16 18:46:48 +0800

    auto-save 18:46:48

--- Personalized_Refusal_VLM/experiments/generate_refusal_score.py
+++ Personalized_Refusal_VLM/experiments/generate_refusal_score.py
@@ -258,8 +258,6 @@ def eval_model(args, output_dir):
 
     # Average vision_out_refusal_scores
     print("Vision-based out-of-constraint refusal score: ", np.mean(vision_out_refusal_scores))
-
-
     # Save vision_out_refusal_scores as a pickle file (List)
     with open(f"{save_dir}/vision_out_refusal_scores_{cfg.model_name}.pkl", "wb") as f:
         pickle.dump(vision_out_refusal_scores, f)
