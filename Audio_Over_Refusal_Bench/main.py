06791f1e26155ca58a9d84660fbcad07f42a6d63
autosave <local@autosave>
2026-01-17 00:24:29 +0800

    auto-save 00:24:29

--- Audio_Over_Refusal_Bench/main.py
+++ Audio_Over_Refusal_Bench/main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
         save_path = os.path.join(data_path, 'audio', f'{idx}.mp3')
         # get highlighted text
-        highlighted_text = get_highlighted_text(prompt)
+        # highlighted_text = get_highlighted_text(prompt)
         print("prompt: ", prompt)
         print("highlighted_text: ", highlighted_text)
         # Generate audio
