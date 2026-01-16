772c90a17b12a526081e83706a572a7bcb5288f2
autosave <local@autosave>
2026-01-16 23:19:10 +0800

    auto-save 23:19:10

--- Audio_Over_Refusal_Bench/llm/ALM.py
+++ Audio_Over_Refusal_Bench/llm/ALM.py
@@ -3,8 +3,8 @@ from urllib.request import urlopen
 import librosa
 from transformers import Qwen2AudioForConditionalGeneration, AutoProcessor
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct")
-model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto")
+processor = AutoProcessor.from_pretrained("/gpu02home/jmy5701/gpu/models/Qwen2-Audio-7B-Instruct")
+model = Qwen2AudioForConditionalGeneration.from_pretrained("/gpu02home/jmy5701/gpu/models/Qwen2-Audio-7B-Instruct", device_map="auto")
 
 conversation = [
     {'role': 'system', 'content': 'You are a helpful assistant.'}, 
