43e2ef4fa4b6493bac0418d4b1d968bc41f22502
autosave <local@autosave>
2026-01-17 11:45:47 +0800

    auto-save 11:45:47

--- Audio_Over_Refusal_Bench/test.py
+++ Audio_Over_Refusal_Bench/test.py
@@ -7,10 +7,6 @@ processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct")
 model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto")
 
 conversation = [
-    # {"role": "user", "content": [
-    #     {"type": "audio", "audio_url": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/guess_age_gender.wav"},
-    # ]},
-    # {"role": "assistant", "content": "Yes, the speaker is female and in her twenties."},
     {"role": "user", "content": [
         {"type": "audio", "audio_url": "https://abbottyanginchina.github.io/assets/4.mp3"},
     ]},
