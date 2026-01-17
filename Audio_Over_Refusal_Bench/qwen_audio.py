51f42ace108c179547278b5c183832d540e6a9a2
autosave <local@autosave>
2026-01-18 00:48:01 +0800

    auto-save 00:48:01

--- Audio_Over_Refusal_Bench/qwen_audio.py
+++ Audio_Over_Refusal_Bench/qwen_audio.py
@@ -6,30 +6,25 @@ from transformers import Qwen2AudioForConditionalGeneration, AutoProcessor
 processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct")
 model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto")
 
-conversation = [
-    {"role": "user", "content": [
-        {"type": "audio", "audio_url": "./4.mp3"},
-    ]},
-]
-text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
-audios = []
-for message in conversation:
-    if isinstance(message["content"], list):
-        for ele in message["content"]:
-            if ele["type"] == "audio":
-                audios.append(librosa.load(
-                    # BytesIO(urlopen(ele['audio_url']).read()), 
-                    ele['audio_url'],
-                    sr=processor.feature_extractor.sampling_rate)[0]
-                )
 
-inputs = processor(text=text, audio=audios, return_tensors="pt", padding=True)
-inputs.input_ids = inputs.input_ids.to("cuda")
+def qwen_audio(audio_url):
+    conversation = [
+        {"role": "user", "content": [
+            {"type": "audio", "audio_url": audio_url},
+        ]},
+    ]
+    text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+    audios = []
+    for message in conversation:
+        if isinstance(message["content"], list):
+            for ele in message["content"]:
 
-generate_ids = model.generate(**inputs)
-generate_ids = generate_ids[:, inputs.input_ids.size(1):]
+    inputs = processor(text=text, audio=audios, return_tensors="pt", padding=True)
+    inputs.input_ids = inputs.input_ids.to("cuda")
 
-response = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    generate_ids = model.generate(**inputs)
+    generate_ids = generate_ids[:, inputs.input_ids.size(1):]
 
-print("================================================")
-print(response)
+    response = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+    return response
