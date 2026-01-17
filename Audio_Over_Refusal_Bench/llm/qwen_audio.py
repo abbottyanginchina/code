66b35ed20101e8c4ae34bf0508e97a64c7141106
autosave <local@autosave>
2026-01-18 00:55:40 +0800

    auto-save 00:55:40

--- Audio_Over_Refusal_Bench/llm/qwen_audio.py
+++ Audio_Over_Refusal_Bench/llm/qwen_audio.py
@@ -6,8 +6,8 @@ from urllib.request import urlopen
 import librosa
 from transformers import Qwen2AudioForConditionalGeneration, AutoProcessor
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct")
-model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct", device_map="auto")
+processor = AutoProcessor.from_pretrained("../../models/Qwen2-Audio-7B-Instruct")
+model = Qwen2AudioForConditionalGeneration.from_pretrained("../../models/Qwen2-Audio-7B-Instruct", device_map="auto")
 
 
 def qwen_audio(audio_path):
