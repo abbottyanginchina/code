ecae63bba9be6990ad283568ae7ec827a988a3a7
autosave <local@autosave>
2026-01-18 00:54:35 +0800

    auto-save 00:54:35

--- Audio_Over_Refusal_Bench/llm/qwen_audio.py
+++ Audio_Over_Refusal_Bench/llm/qwen_audio.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     answers_file = os.path.join(output_dir, f"responses.jsonl")
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
     ans_file = open(answers_file, "w")
+    
     len = 200
     for idx in tqdm(range(len), desc="Generating audio", total=len):
         audio_path = os.path.join("../../data/or-bench/audio", f"{idx}.mp3")
