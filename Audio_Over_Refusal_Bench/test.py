ac94db35f1329f891d6ae3a10887bc893bebe619
autosave <local@autosave>
2026-01-17 09:44:31 +0800

    auto-save 09:44:31

--- Audio_Over_Refusal_Bench/test.py
+++ Audio_Over_Refusal_Bench/test.py
@@ -7,7 +7,7 @@ client = OpenAI(
     # 新加坡和北京地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     # 以下是北京地域base_url，如果使用新加坡地域的模型，需要将base_url替换为：https://dashscope-intl.aliyuncs.com/compatible-mode/v1
-    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    base_url="https://dashscope-intl.aliyuncs.com/compatible-mode/v1",
 )
 
 completion = client.chat.completions.create(
