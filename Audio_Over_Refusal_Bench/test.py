1259c18984efb5d2afc632756be4ee04b4eff3fa
autosave <local@autosave>
2026-01-17 11:56:10 +0800

    auto-save 11:56:10

--- Audio_Over_Refusal_Bench/test.py
+++ Audio_Over_Refusal_Bench/test.py
@@ -8,7 +8,7 @@ model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-
 
 conversation = [
     {"role": "user", "content": [
-        {"type": "audio", "audio_url": "https://abbottyanginchina.github.io/assets/4.mp3"},
+        {"type": "audio", "audio_url": "../../data/or-bench/audio/4.mp3"},
     ]},
 ]
 text = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
