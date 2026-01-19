97ba59b1c50457cbc569548c0f58903cd8fa3dc2
autosave <local@autosave>
2026-01-20 05:42:50 +0800

    auto-save 05:42:50

--- Personalized_Refusal_VLM/evaluation/vision_ablation.py
+++ Personalized_Refusal_VLM/evaluation/vision_ablation.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     for model_name in ["llava-1.5-7b-hf"]:
         dataset_name = "ScienceQA"
         for subject in ["biology", "geography", "physics"]:
-            draw_04(model_name, dataset_name, subject)
+            draw(model_name, dataset_name, subject)
 
         dataset_name = "MMMU"
         for subject in ["Math", "Art_Theory", "Geography"]:
-            draw_04(model_name, dataset_name, subject)
+            draw(model_name, dataset_name, subject)
