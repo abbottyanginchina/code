832b6f9e2ba062c29a3f75c0977ec1648ef4f242
autosave <local@autosave>
2026-01-20 09:12:51 +0800

    auto-save 09:12:51

--- Personalized_Refusal_VLM/evaluation/answer_quality.py
+++ Personalized_Refusal_VLM/evaluation/answer_quality.py
@@ -26,7 +26,6 @@ def chat_LLM(user_prompt):
 if __name__ == '__main__':
     # read .jsonl file
     #/home/ubuntu/jiaxi/results/output_llava-1.5-7b-hf_ScienceQA_biology/results/biology_answer_llava-1.5-7b-hf.jsonl
-    method = "our_method"
     base_dir = "/home/ubuntu/jiaxi/results_gpu01"
     model_name = "llava-1.5-13b-hf"
     files = [f"results/biology_answer_{model_name}.jsonl", f"results/nonbiology_answer_{model_name}.jsonl"]
