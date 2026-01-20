648fc930f303068a855d731dccee83944f184fba
autosave <local@autosave>
2026-01-20 09:13:06 +0800

    auto-save 09:13:06

--- Personalized_Refusal_VLM/evaluation/answer_quality.py
+++ Personalized_Refusal_VLM/evaluation/answer_quality.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     # read .jsonl file
     #/home/ubuntu/jiaxi/results/output_llava-1.5-7b-hf_ScienceQA_biology/results/biology_answer_llava-1.5-7b-hf.jsonl
     base_dir = "/home/ubuntu/jiaxi/results_gpu01"
-    model_name = "llava-1.5-13b-hf"
+    model_name = "llava-1.5-7b-hf"
     files = [f"results/biology_answer_{model_name}.jsonl", f"results/nonbiology_answer_{model_name}.jsonl"]
     tasks = {
         # "ScienceQA": ["biology", "physics", "geography"],
