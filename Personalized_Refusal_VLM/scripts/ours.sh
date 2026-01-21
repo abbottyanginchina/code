d0a0ba08b25799f7c2e076b803261b5b06bd185e
autosave <local@autosave>
2026-01-22 06:21:10 +0800

    auto-save 06:21:10

--- Personalized_Refusal_VLM/scripts/ours.sh
+++ Personalized_Refusal_VLM/scripts/ours.sh
@@ -9,5 +9,5 @@ bash scripts/run_llava_1.5_13b_hf.sh
 echo "Running InstructBLIP..."
 bash scripts/run_instructBLIP.sh
 
-echo "Running llava-1.5-7b-hf..."
-bash scripts/run_llava_1.5_7b_hf.sh
+# echo "Running llava-1.5-7b-hf..."
+# bash scripts/run_llava_1.5_7b_hf.sh
