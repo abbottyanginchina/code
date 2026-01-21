0fe2d17cb5bdd54124bb90c907de19c92831cfc3
autosave <local@autosave>
2026-01-22 06:21:03 +0800

    auto-save 06:21:03

--- Personalized_Refusal_VLM/scripts/ours.sh
+++ Personalized_Refusal_VLM/scripts/ours.sh
@@ -3,9 +3,6 @@
 echo "Running Idefics3-8B-Llama3..."
 bash scripts/run_idefics3.sh
 
-echo "Running llava-1.5-7b-hf..."
-bash scripts/run_llava_1.5_7b_hf.sh
-
 echo "Running llava-1.5-13b-hf..."
 bash scripts/run_llava_1.5_13b_hf.sh
 
