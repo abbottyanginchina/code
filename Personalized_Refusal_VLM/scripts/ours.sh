6cfb6edc8cbeb2096ea3ecf13fa0a9474340e7f2
autosave <local@autosave>
2026-01-22 06:14:50 +0800

    auto-save 06:14:50

--- Personalized_Refusal_VLM/scripts/ours.sh
+++ Personalized_Refusal_VLM/scripts/ours.sh
@@ -3,11 +3,11 @@ export CUDA_VISIBLE_DEVICES=1
 echo "Running InstructBLIP..."
 bash scripts/run_instructBLIP.sh
 
+echo "Running Idefics3-8B-Llama3..."
+bash scripts/run_idefics3.sh
+
 echo "Running llava-1.5-7b-hf..."
 bash scripts/run_llava_1.5_7b_hf.sh
 
 echo "Running llava-1.5-13b-hf..."
 bash scripts/run_llava_1.5_13b_hf.sh
-
-echo "Running Idefics3-8B-Llama3..."
-bash scripts/run_idefics3.sh
