ddc21336270381c2807af8cf86ae15a37260895a
autosave <local@autosave>
2026-01-22 06:14:34 +0800

    auto-save 06:14:34

--- Personalized_Refusal_VLM/scripts/ours.sh
+++ Personalized_Refusal_VLM/scripts/ours.sh
@@ -9,5 +9,5 @@ bash scripts/run_llava_1.5_13b_hf.sh
 echo "Running Idefics3-8B-Llama3..."
 bash scripts/run_idefics3.sh
 
-echo "Running Idefics3-8B-Llama3..."
-bash scripts/run_idefics3.sh
+echo "Running InstructBLIP..."
+bash scripts/run_instructBLIP.sh
