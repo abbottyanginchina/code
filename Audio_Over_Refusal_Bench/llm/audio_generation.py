1005bdc2d7db1d33bd70b0d108ea094ad452f85e
autosave <local@autosave>
2026-01-27 09:02:52 +0800

    auto-save 09:02:52

--- Audio_Over_Refusal_Bench/llm/audio_generation.py
+++ Audio_Over_Refusal_Bench/llm/audio_generation.py
@@ -6,7 +6,7 @@ model = Qwen3TTSModel.from_pretrained(
     "../../../models/Qwen3-TTS-12Hz-1.7B-VoiceDesign",
     device_map="cuda:0",
     dtype=torch.bfloat16,
-    attn_implementation="flash_attention_2",
+    attn_implementation="eager",
 )
 
 # single inference
