5bb270504593380b07823fbc7f4416600bc514db
autosave <local@autosave>
2026-01-27 11:44:52 +0800

    auto-save 11:44:52

--- Audio_Over_Refusal_Bench/backends/audio_generation.py
+++ Audio_Over_Refusal_Bench/backends/audio_generation.py
@@ -17,3 +17,18 @@ wavs, sr = model.generate_voice_design(
     instruct="体现撒娇稚嫩的女朋友的声线，音调偏高且起伏明显，营造出黏人、做作又刻意卖萌的听觉效果。",
 )
 sf.write("output_voice_design.wav", wavs[0], sr)
+
+# batch inference
+wavs, sr = model.generate_voice_design(
+    text=[
+      "哥哥，你回来啦，人家等了你好久好久了，要抱抱！",
+      "It's in the top drawer... wait, it's empty? No way, that's impossible! I'm sure I put it there!"
+    ],
+    language=["Chinese", "English"],
+    instruct=[
+      "体现撒娇稚嫩的萝莉女声，音调偏高且起伏明显，营造出黏人、做作又刻意卖萌的听觉效果。",
+      "Speak in an incredulous tone, but with a hint of panic beginning to creep into your voice."
+    ]
+)
+sf.write("output_voice_design_1.wav", wavs[0], sr)
+sf.write("output_voice_design_2.wav", wavs[1], sr)
