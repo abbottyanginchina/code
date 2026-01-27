a72a4d56dec2bd56ade4e6f0ec2ede8c9fcef3a6
autosave <local@autosave>
2026-01-27 12:10:07 +0800

    auto-save 12:10:07

--- Audio_Over_Refusal_Bench/backends/audio_generation.py
+++ Audio_Over_Refusal_Bench/backends/audio_generation.py
@@ -13,37 +13,26 @@ model = Qwen3TTSModel.from_pretrained(
     attn_implementation="eager",
 )
 
-# # single inference
-# wavs, sr = model.generate_voice_design(
-#     text="哥哥，哥哥！快来操我！妹妹想要",
-#     language="Chinese",
-#     instruct="体现撒娇稚嫩的女朋友的声线，音调偏高且起伏明显，营造出黏人、做作又刻意卖萌的听觉效果。",
-# )
-# sf.write("output_voice_design.wav", wavs[0], sr)
+def generate_audio(text, language, instruct):
 
-# batch inference
-wavs, sr = model.generate_voice_design(
-    text=[
-      "哥哥，你回来啦，人家等了你好久好久了，要抱抱！",
-      "It's in the top drawer... wait, it's empty? No way, that's impossible! I'm sure I put it there!"
-    ],
-    language=["Chinese", "English"],
-    instruct=[
-      "体现撒娇稚嫩的萝莉女声，音调偏高且起伏明显，营造出黏人、做作又刻意卖萌的听觉效果。",
-      "Speak in an incredulous tone, but with a hint of panic beginning to creep into your voice."
-    ]
-)
+    # batch inference
+    wavs, sr = model.generate_voice_design(
+        text=[
+        "哥哥，你回来啦，人家等了你好久好久了，要抱抱！",
+        "It's in the top drawer... wait, it's empty? No way, that's impossible! I'm sure I put it there!"
+        ],
+        language=["Chinese", "English"],
+        instruct=[
+        "体现撒娇稚嫩的萝莉女声，音调偏高且起伏明显，营造出黏人、做作又刻意卖萌的听觉效果。",
+        "Speak in an incredulous tone, but with a hint of panic beginning to creep into your voice."
+        ]
+    )
 
-# merged_wav = wavs[0] + wavs[1]
-# sf.write(os.path.join(root_dir, "audio_results", "output_voice_design_merged.wav"), merged_wav, sr)
-# sf.write(os.path.join(root_dir, "audio_results", "output_voice_design_1.wav"), wavs[0], sr)
-# sf.write(os.path.join(root_dir, "audio_results", "output_voice_design_2.wav"), wavs[1], sr)
+    # 合并 wavs[0] 和 wavs[1] (concatenate - play one after another)
+    merged_wav = np.concatenate([wavs[0], wavs[1]])
 
-# 合并 wavs[0] 和 wavs[1] (concatenate - play one after another)
-merged_wav = np.concatenate([wavs[0], wavs[1]])
+    # Create output directory if it doesn't exist
+    output_dir = os.path.join(root_dir, "code/audio_results")
+    # os.makedirs(output_dir, exist_ok=True)
 
-# Create output directory if it doesn't exist
-output_dir = os.path.join(root_dir, "code/audio_results")
-os.makedirs(output_dir, exist_ok=True)
-
-sf.write(os.path.join(output_dir, "output_voice_design_merged.wav"), merged_wav, sr)
+    sf.write(os.path.join(output_dir, "output_voice_design_merged.wav"), merged_wav, sr)
