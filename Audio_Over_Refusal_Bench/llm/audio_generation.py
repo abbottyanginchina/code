e0e2867b500c7049d7987060a869edf398903fc5
autosave <local@autosave>
2026-01-27 08:57:55 +0800

    auto-save 08:57:55

--- Audio_Over_Refusal_Bench/llm/audio_generation.py
+++ Audio_Over_Refusal_Bench/llm/audio_generation.py
@@ -3,7 +3,7 @@ import soundfile as sf
 from qwen_tts import Qwen3TTSModel
 
 model = Qwen3TTSModel.from_pretrained(
-    "Qwen/Qwen3-TTS-12Hz-1.7B-VoiceDesign",
+    "../../models/Qwen3-TTS-12Hz-1.7B-VoiceDesign",
     device_map="cuda:0",
     dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
