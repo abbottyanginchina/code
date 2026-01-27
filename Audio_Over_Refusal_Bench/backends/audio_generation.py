e2e60fcaeb5088bb9ea8d5769b2959787cf11fc3
autosave <local@autosave>
2026-01-27 11:44:14 +0800

    auto-save 11:44:14

--- Audio_Over_Refusal_Bench/backends/audio_generation.py
+++ Audio_Over_Refusal_Bench/backends/audio_generation.py
@@ -4,7 +4,7 @@ from pydub import AudioSegment
 from qwen_tts import Qwen3TTSModel
 
 model = Qwen3TTSModel.from_pretrained(
-    "\/gpu02home/jmy5701/gpu/models/Qwen3-TTS-12Hz-1.7B-VoiceDesign",
+    "/gpu02home/jmy5701/gpu/models/Qwen3-TTS-12Hz-1.7B-VoiceDesign",
     device_map="cuda:0",
     dtype=torch.bfloat16,
     attn_implementation="eager",
