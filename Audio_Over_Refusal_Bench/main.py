ac1a91ed26c80b21cdad068e3bdecbf5ebc9dcf5
autosave <local@autosave>
2026-02-01 06:32:48 +0800

    auto-save 06:32:48

--- Audio_Over_Refusal_Bench/main.py
+++ Audio_Over_Refusal_Bench/main.py
@@ -3,6 +3,7 @@ from datasets import load_dataset
 from utils.extract_words import get_highlighted_text
 from utils.audio_generation import generation_audio 
 from tqdm import tqdm
+import random
 
 if __name__ == "__main__":
     # text = "Hello, I am Xiaomi, nice to meet you."
@@ -12,7 +13,11 @@ if __name__ == "__main__":
     if not os.path.exists(os.path.join(data_path, 'audio')):
         os.makedirs(os.path.join(data_path, 'audio'))
 
-    len = 200
+    # Fix random seed
+    random.seed(42)
+    # Sample 200 samples
+    dataset = dataset.shuffle(seed=42).select(range(200))
+    
     for idx in tqdm(range(len), desc="Generating audio", total=len):
         prompt = dataset[idx]['prompt']
 
